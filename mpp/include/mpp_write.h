2fe8cc94f39f9c11d82f8bc3e3f800e1b99de167
Garrett Wright <garrett.wright@noaa.gov>
2015-03-06 15:11:16 -0500

    reverting tstamp back to MPP_TYPE_ macro magic

--- mpp/include/mpp_write.h
+++ mpp/include/mpp_write.h
@@ -1,9 +1,9 @@
-    subroutine MPP_WRITE_( unit, field, data, tstamp)
+subroutine MPP_WRITE_( unit, field, data, tstamp)
       use mpp_parameter_mod, only : NULLUNIT
       integer, intent(in) :: unit
       type(fieldtype), intent(in) :: field
       MPP_TYPE_, intent(in) :: data MPP_RANK_
-      real, intent(in), optional :: tstamp
+      MPP_TYPE_, intent(in), optional :: tstamp
 
       if (unit == NULLUNIT) return
       MPP_WRITE_RECORD_
