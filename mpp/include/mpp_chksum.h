407edafbfbb373bf25859a5f2897c5650f82ffbd
Garrett Wright <Garrett.Wright@noaa.gov>
2015-03-09 00:00:00 -0400

    Idealized tests used complex(8) types
    which conflicted with sizing mold type. Reverting to Ulm (and prior)
    forcing of size to LONG_KIND.

--- mpp/include/mpp_chksum.h
+++ mpp/include/mpp_chksum.h
@@ -1,11 +1,20 @@
-    function MPP_CHKSUM_( var, pelist )
+function MPP_CHKSUM_( var, pelist , mask_val)
 !mold is a dummy array to be used by TRANSFER()
 !must be same TYPE as result
 !result is LONG_KIND, which will actually be int ifdef no_8byte_integers
-      integer(LONG_KIND) :: MPP_CHKSUM_, mold(1)
+  !optional mask_val is masked away in checksum_int.h function via PACK()
+  integer(LONG_KIND) :: MPP_CHKSUM_
+  integer(LONG_KIND) :: mold(1)
       MPP_TYPE_, intent(in) :: var MPP_RANK_
       integer, intent(in), optional :: pelist(:)
+  MPP_TYPE_, intent(in),optional :: mask_val
 
+  if ( PRESENT(mask_val) ) then
+     MPP_CHKSUM_ = mpp_chksum( TRANSFER(var,mold), pelist, &
+          mask_val= TRANSFER(mask_val,mold(1) ) )
+  else
       MPP_CHKSUM_ = mpp_chksum( TRANSFER(var,mold), pelist )
+  end if
+  
       return
     end function MPP_CHKSUM_
