d10584d2a5ce8e4e1592f2d9cbfcb6e25675c6f6
Garrett.Wright <Garrett.Wright@noaa.gov>
2015-08-28 11:28:22 -0400

    comments, err string

--- mpp/include/mpp_chksum_int.h
+++ mpp/include/mpp_chksum_int.h
@@ -33,38 +33,53 @@ function MPP_CHKSUM_INT_RMASK_( var, pelist, mask_val )
   !high fidelity error message
   character(LEN=1) :: tmpStr1,tmpStr2,tmpStr3
   character(LEN=32) :: tmpStr4
-  character(LEN=256) :: errStr
+  character(LEN=512) :: errStr
 
-
-  ! typically should catch real "default_fill" .OR. mask_val == MPP_FILL_FLOAT
+  
+! Primary Logic
+  !!Should catch real "default_fill", .OR. mask_val == MPP_FILL_FLOAT 
   if (mask_val == MPP_FILL_DOUBLE ) then
-     !use MPP_FILL_INT
+     ! we've packed an MPP_FILL_
      imask_val = MPP_FILL_INT
+  !! Should catch alternative storage representations
+  !!! Current NETCDF fill values (AKA MPP_FILL_*) designed towards CEILING(MPP_FILL_{FLOAT,DOUBLE},kind=4byte)=MPP_FILL_INT
   else if ( CEILING(mask_val,INT_KIND) == MPP_FILL_INT ) then
-     ! NETCDF fill values proveide CEILING(MPP_FILL_{FLOAT,DOUBLE},kind=4byte)=MPP_FILL_INT
+     ! we've packed an MPP_FILL_
      imask_val = MPP_FILL_INT
-  else if ( KIND(mask_val) == KIND(var) ) then
-     ! same size see if previously ducktyped integere via transfer
+! Secondary Logic, Corner Cases
+  !! Same KIND, works when parity between *_KINDS across REAL and INTEGER
+  else if ( KIND(mask_val) == KIND(var) ) then 
+     ! see if previously ducktyped integer via transfer
      itmpVarP(1) = TRANSFER(mask_val , itmpVarP(1))
-     if ( itmpVarP(1) == MPP_FILL_INT ) imask_val = MPP_FILL_INT
-  else if ( KIND(mask_val)==DOUBLE_KIND .AND. KIND(var)==INT_KIND ) then
-     itmpVarP = TRANSFER(mask_val, itmpVarP)
-     ! did we pack int fill
-     if ( ANY(itmpVarP == MPP_FILL_INT ) .OR. ANY( itmpVarP == CEILING(MPP_FILL_DOUBLE,LONG_KIND) ) ) then
+     if ( itmpVarP(1) == MPP_FILL_INT ) then 
+        ! we've packed an MPP_FILL_
         imask_val = MPP_FILL_INT
-     else ! did we pack real fill
+     end if
+  !! r8 mask, i4 var
+  else if ( KIND(mask_val)==DOUBLE_KIND .AND. KIND(var)==INT_KIND ) then 
+     itmpVarP = TRANSFER(mask_val, itmpVarP)
+     ! see if previously ducktyped integer via transfer into either long word
+     ! .OR. if packed the double as a long long (two words)
+     if ( ANY(itmpVarP == MPP_FILL_INT ) .OR. ANY( itmpVarP == CEILING(MPP_FILL_DOUBLE,LONG_KIND) ) ) then
+        ! we've packed an MPP_FILL_
+        imask_val = MPP_FILL_INT
+     else ! see if previously packed float via transfer into either long word
         rtmpVarP = TRANSFER(mask_val, rtmpVarP)
         if ( ANY(rtmpVarP == MPP_FILL_DOUBLE ) .OR. ANY(CEILING(rtmpVarP,INT_KIND) == MPP_FILL_INT) ) then
+           ! we've packed an MPP_FILL_
            imask_val = MPP_FILL_INT
         end if
      end if
-! here be dragons, we should not be doing either of these things
-  else if ( KIND(mask_val)==FLOAT_KIND .AND. KIND(var)==LONG_KIND ) then
-     call mpp_error(NOTE, "Your have called mpp_chksum with a var(LONG_KIND) and mask_val(FLOAT_KIND) // &
-          and are not using the supported MPP_FILL_ parameters. Taking CEILING(mask_val,LONG_KING),// & 
-          please confirm this is what you want and use MPP_FILL_ to make this message go away.")
+! according to the Walpiri, there should be no third order,
+! so here be dragons, we should not be doing either of these things
+  !! r4 mask, i8 var
+  else if ( KIND(mask_val)==FLOAT_KIND .AND. KIND(var)==LONG_KIND ) then 
+     call mpp_error(NOTE, "mpp_chksum was called with a var(LONG_KIND) and mask_val(FLOAT_KIND) // &
+          and supported MPP_FILL_ parameters were not found. MPP will continue using CEILING(mask_val,LONG_KING);// & 
+          to support possible legacy code. Please contact your FMS liason if you need help //&
+          using MPP_FILL_ along with explicit KINDs to make this message go away.")
      imask_val = CEILING(mask_val,KIND=LONG_KIND)
-  else 
+  else ! 
      ! construct detailed errStr
       errStr = "mpp_chksum: mpp_chksum_i" 
       write(unit=tmpStr1,fmt="(I1)") KIND(var) 
@@ -73,7 +88,8 @@ function MPP_CHKSUM_INT_RMASK_( var, pelist, mask_val )
       write(unit=tmpstr3,fmt="(I1)") KIND(mask_val) 
       errStr = errStr // tmpstr3 // ") mask_val="
       write(unit=tmpstr4,fmt=*) mask_val
-      errStr = errStr // trim(tmpstr4) // ", andthis is not a supported cast. Check _FillValue and mask_val. Hint: Try MPP_FILL_{INT,FLOAT,DOUBLE}. Defaulting to MPP_FILL_INT."
+      errStr = errStr // trim(tmpstr4) // "has been called with these strange values. Check your KINDS, _FillValue, pack and mask_val. // &
+           Hint: Try being explicit and using MPP_FILL_{INT,FLOAT,DOUBLE}. Continuing by using the default MPP_FILL_INT."
       call mpp_error(NOTE, trim(errStr) ) 
 
       imask_val = MPP_FILL_INT
