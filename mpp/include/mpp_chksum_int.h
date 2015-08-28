cfebbea8bc09f45931c445654fed46e604c4e67c
Garrett.Wright <Garrett.Wright@noaa.gov>
2015-08-28 09:41:57 -0400

    changed branching

--- mpp/include/mpp_chksum_int.h
+++ mpp/include/mpp_chksum_int.h
@@ -26,26 +26,60 @@ function MPP_CHKSUM_INT_RMASK_( var, pelist, mask_val )
   MPP_TYPE_, intent(in) :: var MPP_RANK_
   integer, optional :: pelist(:)
   real, intent(in) :: mask_val
-  integer(KIND(var))::tmpVarP
-  integer(KIND(mask_val)) :: tmpFullP,tmpZeroMasked
+  integer(KIND(var))::imask_val
+  integer(KIND(var))::itmpVarP(2)
+  real(KIND(var))::rtmpVarP(2)
+  !!integer(KIND(mask_val)) :: tmpFullP,tmpZeroMasked
+  !high fidelity error message
+  character(LEN=1) :: tmpStr1,tmpStr2,tmpStr3
+  character(LEN=32) :: tmpStr4
+  character(LEN=256) :: errStr
 
 
-  if ( KIND(mask_val) == KIND(var)) then
-     !same numBytes
-     !cast to MPP_TYPE_
-     tmpVarP = TRANSFER(mask_val , tmpVarP)
-  else if (KIND(mask_val) /=  KIND(var) ) then
-     !check if still safe to duck type to lower precision
-     tmpFullP = TRANSFER(mask_val,tmpFullP ) ! transfer bits to int mold of same numBytes
-     tmpZeroMasked = IBITS(tmpFullP,0,BIT_SIZE(tmpVarP))
-     if (tmpFullP /= tmpZeroMasked ) then
-        !as an int, mask_val is actually /using/ higher bits than var, and so not a valid mask...
-        call mpp_error(FATAL, "mpp_chksum_int.h was called with real mask_val, and mask_val can not be safely cast to int type of var (nonzero high bits).")
+  ! typically should catch real "default_fill"
+  if (mask_val == MPP_FILL_DOUBLE .OR. mask_val == MPP_FILL_FLOAT) then
+     !use MPP_FILL_INT
+     imask_val = MPP_FILL_INT
+  else if ( CEILING(mask_val,INT_KIND) == MPP_FILL_INT ) then
+     ! NETCDF fill values proveide CEILING(MPP_FILL_{FLOAT,DOUBLE},kind=4byte)=MPP_FILL_INT
+     imask_val = MPP_FILL_INT
+  else if ( KIND(mask_val) == KIND(var) ) then
+     ! same size see if previously ducktyped via transfer
+     itmpVarP(1) = TRANSFER(mask_val , itmpVarP(1))
+     if ( itmpVarP(1) == MPP_FILL_INT .OR. CEILING(itmpVarP(1),INT_KIND) == MPP_FILL_INT) imask_val = MPP_FILL_INT
+  else if ( KIND(mask_val)==DOUBLE_KIND .AND. KIND(var)==INT_KIND ) then
+     itmpVarP = TRANSFER(mask_val, itmpVarP)
+     ! did we pack int fill
+     if ( ANY(itmpVarP == MPP_FILL_INT ) .OR. ANY(CEILING(itmpVarP,INT_KIND) == MPP_FILL_INT)) then
+        imask_val = MPP_FILL_INT
+     else ! did we pack real fill
+        rtmpVarP = TRANSFER(mask_val, rtmpVarP)
+        if ( ANY(rtmpVarP == MPP_FILL_FLOAT ) .OR. ANY(CEILING(rtmpVarP,INT_KIND) == MPP_FILL_INT) ) then
+           imask_val = MPP_FILL_INT
+        end if
      end if
-     tmpVarP = INT(tmpFullP, KIND(var) ) ! attempt cast of mask_val as int down to the int precision of var, could do transfer...
-  end if
+! here be dragons, we should not be doing either of these things
+  else if ( KIND(mask_val)==FLOAT_KIND .AND. KIND(var)==LONG_KIND ) then
+     call mpp_error(NOTE, "Your have called mpp_chksum with a var(LONG_KIND) and mask_val(FLOAT_KIND) // &
+          and are not using the supported MPP_FILL_ parameters. Taking CEILING(mask_val,LONG_KING),// & 
+          please confirm this is what you want and use MPP_FILL_ to make this message go away.")
+     imask_val = CEILING(mask_val,KIND=LONG_KIND)
+  else 
+     ! construct detailed errStr
+      errStr = "mpp_chksum: mpp_chksum_i" 
+      write(unit=tmpStr1,fmt="(I1)") KIND(var) 
+      write(unit=tmpstr2,fmt="(I1)") SIZE(SHAPE(var))
+      errStr = errStr // tmpStr1 // "_" // tmpstr2 // "d_rmask passed int var with REAL(" 
+      write(unit=tmpstr3,fmt="(I1)") KIND(mask_val) 
+      errStr = errStr // tmpstr3 // ") mask_val="
+      write(unit=tmpstr4,fmt=*) mask_val
+      errStr = errStr // trim(tmpstr4) // ", andthis is not a supported cast. Check _FillValue and mask_val. Hint: Try MPP_FILL_{INT,FLOAT,DOUBLE}. Defaulting to MPP_FILL_INT."
+      call mpp_error(NOTE, trim(errStr) ) 
 
-  MPP_CHKSUM_INT_RMASK_ = mpp_chksum(var,pelist,mask_val=tmpVarP)
+      imask_val = MPP_FILL_INT
+   end if
+
+  MPP_CHKSUM_INT_RMASK_ = mpp_chksum(var,pelist,mask_val=imask_val)
 
   return
 
