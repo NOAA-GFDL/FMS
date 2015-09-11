46578251d8175df141d7e67347381cb52cda09fc
Garrett.Wright <Garrett.Wright@noaa.gov>
2015-09-11 10:17:57 -0400

    removed extraneous line, comment detail, using WARNING with detailed message, prepping for FATAL move post cvs Land-dev exodus

--- mpp/include/mpp_chksum_int.h
+++ mpp/include/mpp_chksum_int.h
@@ -29,16 +29,16 @@ function MPP_CHKSUM_INT_RMASK_( var, pelist, mask_val )
   integer(KIND(var))::imask_val
   integer(KIND(var))::itmpVarP(2)
   real(KIND(var))::rtmpVarP(2)
-  !!integer(KIND(mask_val)) :: tmpFullP,tmpZeroMasked
   !high fidelity error message
   character(LEN=1) :: tmpStr1,tmpStr2,tmpStr3
   character(LEN=32) :: tmpStr4
   character(LEN=512) :: errStr
 
 
-! Primary Logic
-  !!Should catch real "default_fill", .OR. mask_val == MPP_FILL_FLOAT
-  if (mask_val == MPP_FILL_DOUBLE ) then
+! Primary Logic: These are the "expected" branches.
+!! These all resolve to MPP_FILL_INT
+  !!Should catch real "default_fill"(MPP_FILL_DOUBLE), .OR. mask_val == MPP_FILL_FLOAT
+  if (mask_val == MPP_FILL_DOUBLE ) then !this is FMS variable default fill, and is the crutch supporting nonregistered...
      ! we've packed an MPP_FILL_
      imask_val = MPP_FILL_INT
   !! Should catch alternative storage representations
@@ -46,7 +46,8 @@ function MPP_CHKSUM_INT_RMASK_( var, pelist, mask_val )
   else if ( CEILING(mask_val,INT_KIND) == MPP_FILL_INT ) then
      ! we've packed an MPP_FILL_
      imask_val = MPP_FILL_INT
-! Secondary Logic, Corner Cases
+! Secondary Logic: First Class Corner Cases
+!! These all resolve to MPP_FILL_INT
   !! Same KIND, works when parity between *_KINDS across REAL and INTEGER
   else if ( KIND(mask_val) == KIND(var) ) then
      ! see if previously ducktyped integer via transfer
@@ -59,7 +60,7 @@ function MPP_CHKSUM_INT_RMASK_( var, pelist, mask_val )
   else if ( KIND(mask_val)==DOUBLE_KIND .AND. KIND(var)==INT_KIND ) then
      itmpVarP = TRANSFER(mask_val, itmpVarP)
      ! see if previously ducktyped integer via transfer into either long word
-     ! .OR. if packed the double as a long long (two words)
+     ! .OR. if packed the double as a long long (two words), future MPP_FILL_LONG when NETCDF pushes int64_t
      if ( ANY(itmpVarP == MPP_FILL_INT ) .OR. ANY( itmpVarP == CEILING(MPP_FILL_DOUBLE,LONG_KIND) ) ) then
         ! we've packed an MPP_FILL_
         imask_val = MPP_FILL_INT
@@ -70,14 +71,14 @@ function MPP_CHKSUM_INT_RMASK_( var, pelist, mask_val )
            imask_val = MPP_FILL_INT
         end if
      end if
-! according to the Walpiri, there should be no third order,
-! so here be dragons, we should not be doing either of these things
+! according to the Walpiri, there should be no third logic, it represent too many, ie low class corner cases..
+! so here be dragons, we should not be doing either of these things, but someone will try, and we are going to help them
   !! r4 mask, i8 var
   else if ( KIND(mask_val)==FLOAT_KIND .AND. KIND(var)==LONG_KIND ) then
-     call mpp_error(NOTE, "mpp_chksum was called with a var(LONG_KIND) and mask_val(FLOAT_KIND) // &
+     call mpp_error(WARNING, "mpp_chksum was called with a var(LONG_KIND) and mask_val(FLOAT_KIND) // &
           and supported MPP_FILL_ parameters were not found. MPP will continue using CEILING(mask_val,LONG_KING);// &
           to support possible legacy code. Please contact your FMS liason if you need help //&
-          using MPP_FILL_ along with explicit KINDs to make this message go away.")
+          using MPP_FILL_ along with explicit KINDs to make this message go away. THIS WILL BE FATAL IN THE FUTURE!")
      imask_val = CEILING(mask_val,KIND=LONG_KIND)
   else !
      ! construct detailed errStr
@@ -89,8 +90,9 @@ function MPP_CHKSUM_INT_RMASK_( var, pelist, mask_val )
       errStr = errStr // tmpstr3 // ") mask_val="
       write(unit=tmpstr4,fmt=*) mask_val
       errStr = errStr // trim(tmpstr4) // "has been called with these strange values. Check your KINDS, _FillValue, pack and mask_val. // &
-           Hint: Try being explicit and using MPP_FILL_{INT,FLOAT,DOUBLE}. Continuing by using the default MPP_FILL_INT."
-      call mpp_error(NOTE, trim(errStr) )
+           Hint: Try being explicit and using MPP_FILL_{INT,FLOAT,DOUBLE}. Continuing by using the default MPP_FILL_INT. // &
+           THIS WILL BE FATAL IN THE FUTURE!"
+      call mpp_error(WARNING, trim(errStr) )
 
       imask_val = MPP_FILL_INT
    end if
