305ce145c4fd51cb5231c6ca12c7b58e6fa259c6
Niki Zadeh <Niki.Zadeh@noaa.gov>
2018-01-09 15:29:41 -0500

    Adding Nitrogen molecular weight
    - The value comes from ocean_shared/generic_tracers/generic_COBALT.F90
    - It's being used by atmos_shared/tracer_driver/atmos_tracer_driver.F90
    - It's recommended that generic_COBALT.F90 use this value from constants/constants.F90

--- constants/constants.F90
+++ constants/constants.F90
@@ -48,6 +48,7 @@ real, public, parameter :: WTMCO2   =  44.00995 !< Molecular weight of carbon di
 real, public, parameter :: WTMO2    =  31.9988 !< Molecular weight of molecular oxygen [AMU]
 real, public, parameter :: WTMCFC11 = 137.3681 !< Molecular weight of CFC-11 (CCl3F) [AMU]
 real, public, parameter :: WTMCFC12 = 120.9135 !< Molecular weight of CFC-21 (CCl2F2) [AMU]
+real, public, parameter :: MW_N     =  14.0067  !< Molecular weight of Nitrogen [g/mol]
 real, public, parameter :: DIFFAC = 1.660000E+00 !< Diffusivity factor [dimensionless]
 real, public, parameter :: SECONDS_PER_DAY  = 8.640000E+04 !< Seconds in a day [s]
 real, public, parameter :: SECONDS_PER_HOUR = 3600. !< Seconds in an hour [s]
@@ -73,6 +74,7 @@ real, public, parameter :: RADIAN  = RAD_TO_DEG !< Equal to RAD_TO_DEG. Named RA
 real, public, parameter :: C2DBARS = 1.e-4 !< Converts rho*g*z (in mks) to dbars: 1dbar = 10^4 (kg/m^3)(m/s^2)m [dbars]
 real, public, parameter :: KELVIN  = 273.15 !< Degrees Kelvin at zero Celsius [K]
 real, public, parameter :: EPSLN   = 1.0e-40 !< A small number to prevent divide by zero exceptions [N/A]
+
 !-----------------------------------------------------------------------
 public :: constants_init
 
