133398a7e479b291766bce8aa23675b00ed146c1
Ed Hartnett <edwardjameshartnett@gmail.com>
2019-02-27 06:41:41 -0700

    added first test

---
+++ test_fms/tst_fms1.F90
@@ -0,0 +1,13 @@
+! This is a sample test program for the FMS library.
+
+      program tst_fms1
+!      use netcdf4_f03
+      implicit none
+
+      print *, ''
+      print *,'*** Testing FMS library...'
+
+      ! Insert test code here.
+
+      print *,'*** SUCCESS!'
+      end
