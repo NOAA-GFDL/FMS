e7bbc0c9b259548ed979efd3c4629b030aec07a1
Uriel Ramirez <uramirez8707@gmail.com>
2019-05-13 13:13:59 -0400

    fft: Added the LGPL header to the .F90 file, the test now fails if there is an error

--- test_fms/fft/test_fft.F90
+++ test_fms/fft/test_fft.F90
@@ -1,5 +1,26 @@
+!***********************************************************************
+!*                   GNU Lesser General Public License
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* FMS is free software: you can redistribute it and/or modify it under
+!* the terms of the GNU Lesser General Public License as published by
+!* the Free Software Foundation, either version 3 of the License, or (at
+!* your option) any later version.
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+!* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+!* for more details.
+!*
+!* You should have received a copy of the GNU Lesser General Public
+!* License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+!***********************************************************************
+
 program test_fft
 use fft_mod
+use mpp_mod,       only : mpp_error, FATAL, stdout
+
 integer, parameter :: lot = 2
 real   , allocatable :: ain(:,:), aout(:,:)
 complex, allocatable :: four(:,:)
@@ -25,9 +46,11 @@ integer :: ntrans(2) = (/ 60, 90 /)
     do j=1,lot
     do i=1,n
       write (*,'(2i4,3(2x,f15.9))') j, i, ain(i,j), aout(i,j), aout(i,j)-ain(i,j)
-    enddo
-    enddo
+      if (abs(aout(i,j)-ain(i,j)) > 1.0e-8) call mpp_error(FATAL, 'test_fft: the difference between the transformed and original grid is too large')
 
+    enddo
+    enddo
+   
     call fft_end
     deallocate (ain,aout,four)
   enddo
