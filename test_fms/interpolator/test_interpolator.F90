43daa508c3f7a5f1b89bacc8e7e7f22dfc894cac
Uriel Ramirez <uramirez8707@gmail.com>
2019-05-15 14:00:47 -0400

    interpolator: Added the LGPL header, added the diag table required

--- test_fms/interpolator/test_interpolator.F90
+++ test_fms/interpolator/test_interpolator.F90
@@ -1,4 +1,39 @@
-program test
+!***********************************************************************
+!*                   GNU Lesser General Public License
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* FMS is free software: you can redistribute it and/or modify it under
+!* the terms of the GNU Lesser General Public License as published by
+!* the Free Software Foundation, either version 3 of the License, or (at
+!* your option) any later version.
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+!* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+!* for more details.
+!*
+!* You should have received a copy of the GNU Lesser General Public
+!* License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+!***********************************************************************
+
+!***********************************************************************
+!* Requires namelist but it can be empty. Requires a diag_table such as 
+!* below, requires the input files: (aerosol.climatology.nc and 
+!* o3.climatology.nc)
+
+!* diag_table
+!* test_diag_manager_01
+!* 1 3 1 0 0 0
+
+!* #output files
+!*  "diag_test_01",  1, "days", 1, "days", "time"
+
+!* #output variables
+!* "test_diag_manager_mod", "dat1", "dat1", "diag_test_01",  "all", .false., "none", 2
+!***********************************************************************
+
+program test_interpolator
 
 #ifdef INTERNAL_FILE_NML
 use mpp_mod, only: input_nml_file
@@ -175,7 +210,6 @@ column_diagnostic_id(i) =  register_diag_field('interp',colaer,axes(1:2),model_t
                                 'column_aerosol_clim', 'kg/m2', missing_value)
 enddo
 
-
 call ozone_init(o3,lonb_mod(isd:ied+1,jsd:jed+1), latb_mod(isd:ied+1,jsd:jed+1), axes, model_time, &
                 data_out_of_bounds=out_of_bounds)
 
@@ -305,4 +339,4 @@ call interpolator( o3, model_time, p_half, model_data, "ozone", is, js)
 
 end subroutine get_ozone
 
-end program test
+end program test_interpolator
