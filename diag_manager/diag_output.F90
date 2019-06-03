6df71e9c15332cf13d01e040c2c3920212e04489
Thomas Robinson <Thomas.Robinson@noaa.gov>
2019-06-03 14:50:29 -0400

    Adds edge metadata and edge axes to the history files using fms2_io

--- diag_manager/diag_output.F90
+++ diag_manager/diag_output.F90
@@ -277,7 +277,7 @@ CONTAINS
 
     TYPE(domainUG)       :: domainU
 
-    CHARACTER(len=mxch)  :: axis_name, axis_units
+    CHARACTER(len=mxch)  :: axis_name, axis_units, axis_name_current
     CHARACTER(len=mxchl) :: axis_long_name
     CHARACTER(len=1)     :: axis_cart_name
     INTEGER              :: axis_direction, axis_edges
@@ -296,6 +296,7 @@ integer :: domain_size, axis_length, axis_pos
     integer(INT_KIND),dimension(:),allocatable :: io_pelist
     integer(INT_KIND),dimension(:),allocatable :: unstruct_axis_sizes
     real,dimension(:),allocatable              :: unstruct_axis_data
+    integer                                    :: id_axis_current
     logical :: is_time_axis_registered
     integer :: istart, iend
     integer :: data_size
@@ -623,6 +624,8 @@ integer :: domain_size, axis_length, axis_pos
        IF ( axis_edges <= 0 ) CYCLE
 
        !  --- was this axis edge previously defined? ---
+       id_axis_current = id_axis
+       axis_name_current = axis_name
        id_axis = axis_edges
        edges_index = get_axis_index(id_axis)
        IF ( edges_index > 0 ) CYCLE
@@ -636,7 +639,7 @@ integer :: domain_size, axis_length, axis_pos
        !  ---- write edges attribute to original axis ----
        CALL mpp_write_meta(file_unit, mpp_get_id(Axis_types(num_axis_in_file)),&
             & 'edges', cval=axis_name )
-!if (present(fileob)) call register_variable_attribute(fileob, mpp_get_id(Axis_types(num_axis_in_file)), 'edges', trim(axis_name))
+       call register_variable_attribute(fileob, axis_name_current, "edges",trim(axis_name))
        !  ---- add edges index to axis list ----
        !  ---- assume this is not a time axis ----
        num_axis_in_file = num_axis_in_file + 1
@@ -671,7 +674,61 @@ integer :: domain_size, axis_length, axis_pos
           CALL mpp_write_meta(file_unit, Axis_types(num_axis_in_file), axis_name, axis_units,&
                & axis_long_name, axis_cart_name, axis_direction, DATA=axis_data)
        END IF
+!> Add edges axis with fms2_io
+                if (present(fileob))then
+                 select type (fptr)
+                   type is (FmsNetcdfUnstructuredDomainFile_t)
+                        call register_axis(fptr, axis_name, size(axis_data) )
+                        call register_field(fptr, axis_name, "double", (/axis_name/) )
+                        call register_variable_attribute(fptr, axis_name, "units", axis_units)
+                        call register_variable_attribute(fptr, axis_name, "long_name", axis_long_name)
+                        call register_variable_attribute(fptr, axis_name, "cartesian_axis",trim(axis_cart_name))
+                        select case (axis_direction)
+                             case (1)
+                                  call register_variable_attribute(fptr, axis_name, "positive", "up")
+                             case (-1)
+                                  call register_variable_attribute(fptr, axis_name, "positive", "down")
+                        end select
+                        call write_data(fptr, axis_name, axis_data)
+                   type is (FmsNetcdfDomainFile_t)
+                    if (.not.variable_exists(fptr, axis_name)) then
+                        call register_axis(fptr, axis_name, size(axis_data) )
+                        call register_field(fptr, axis_name, "double", (/axis_name/) )
+                        call register_variable_attribute(fptr, axis_name, "units", axis_units)
+                        call register_variable_attribute(fptr, axis_name, "long_name", axis_long_name)
+                        call register_variable_attribute(fptr, axis_name, "cartesian_axis",trim(axis_cart_name))
+                        select case (axis_direction)
+                             case (1)
+                                  call register_variable_attribute(fptr, axis_name, "positive", "up")
+                             case (-1)
+                                  call register_variable_attribute(fptr, axis_name, "positive", "down")
+                        end select
+                        call write_data(fptr, axis_name, axis_data)
+                    endif
+                   type is (FmsNetcdfFile_t)
+                    if (.not.variable_exists(fptr, axis_name)) then
+                        call register_axis(fptr, axis_name, size(axis_data) )
+                        call register_field(fptr, axis_name, "double", (/axis_name/) )
+                        call register_variable_attribute(fptr, axis_name, "units", axis_units)
+                        call register_variable_attribute(fptr, axis_name, "long_name", axis_long_name)
+                        call register_variable_attribute(fptr, axis_name, "cartesian_axis",trim(axis_cart_name))
+                        select case (axis_direction)
+                             case (1)
+                                  call register_variable_attribute(fptr, axis_name, "positive", "up")
+                             case (-1)
+                                  call register_variable_attribute(fptr, axis_name, "positive", "down")
+                        end select
+                        call write_data(fptr, axis_name, axis_data)
+                    endif
+                   class default
+                        call error_mesg("diag_output_mod::write_axis_meta_data", &
+                             "The file object unstructured 2 is not the right type.", FATAL)
+                 end select
+                endif
+
+
 !       call write_axis_edges(fileob, axis_name , axis_edges)
+
        ! Write edge axis attributes
        id_axis = mpp_get_id(Axis_types(num_axis_in_file))
        CALL write_attribute_meta(file_unit, id_axis, num_attributes, attributes, err_msg)
