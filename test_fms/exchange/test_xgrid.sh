c2155a7358d18fe4987153a0fa2be0ae5a1199a5
Uriel Ramirez <uramirez8707@gmail.com>
2019-08-19 16:25:23 -0400

    Attempt to get exchange unit tests to pass make distcheck

--- test_fms/exchange/test_xgrid.sh
+++ test_fms/exchange/test_xgrid.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-export PATH="$PATH:../bats/bin"
-bats test_xgrid.bats
+export PATH="$PATH:$srcdir/test_fms/bats/bin"
+export PATH="$PATH:$srcdir/test_fms/bats/libexec"
+
+cp -r $srcdir/test_fms/exchange/INPUT INPUT
+cp $srcdir/test_fms/exchange/input.nml input.nml
+
+bats $srcdir/test_fms/exchange/test_xgrid.bats
