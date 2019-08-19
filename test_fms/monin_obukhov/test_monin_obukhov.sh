e72e22c1e532162aff225ab54fe25160cd65143c
Uriel Ramirez <uramirez8707@gmail.com>
2019-08-19 16:47:36 -0400

    Attempt to get monin_obukhov unit tests to pass make distcheck

--- test_fms/monin_obukhov/test_monin_obukhov.sh
+++ test_fms/monin_obukhov/test_monin_obukhov.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-export PATH="$PATH:../bats/bin"
-bats test_monin_obukhov.bats
+
+export PATH="$PATH:$srcdir/test_fms/bats/bin"
+export PATH="$PATH:$srcdir/test_fms/bats/libexec"
+
+cp $srcdir/test_fms/monin_obukhov/input.nml input.nml
+
+bats $srcdir/test_fms/monin_obukhov/test_monin_obukhov.bats
