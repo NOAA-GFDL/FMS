6bf039cb62dee0e63716933e79370d664198c244
wrongkindofdoctor <>
2019-09-19 13:45:12 -0400

    added missing close_file to fms_mod module use statement

--- test_fms/horiz_interp/test2_horiz_interp.F90
+++ test_fms/horiz_interp/test2_horiz_interp.F90
@@ -41,7 +41,7 @@ program test2_horiz_interp
   use fms_io_mod,       only : read_data, write_data
   use fms_io_mod,       only : field_size, fms_io_exit, get_mosaic_tile_file
   use fms_mod,          only : fms_init, fms_end, file_exist, field_exist, open_namelist_file
-  use fms_mod,          only : check_nml_error, write_version_number, lowercase
+  use fms_mod,          only : check_nml_error, write_version_number, lowercase, close_file
   use constants_mod,    only : constants_init, PI
   use horiz_interp_mod, only : horiz_interp_new, horiz_interp, horiz_interp_end, horiz_interp_type
   use axis_utils_mod,    only : get_axis_bounds
