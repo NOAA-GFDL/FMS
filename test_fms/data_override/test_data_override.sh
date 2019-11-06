a637a1c6bac9da17ee8312e17d771792404e24bb
edwardhartnett <edwardjameshartnett@gmail.com>
2019-11-06 11:24:42 -0700

    adding set -e in more places

--- test_fms/data_override/test_data_override.sh
+++ test_fms/data_override/test_data_override.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Cause test to fail if any command fails.
+set -e
+
 cp $srcdir/test_fms/data_override/data_table_base data_table
 cp $srcdir/test_fms/data_override/diag_table_base diag_table
 
