dcff228a25fb3912aa312be1ea7fc1ca828b3b10
Edward Hartnett <edward.hartnett@noaa.gov>
2019-11-26 12:54:08 -0700

    shell tests now have TAP output

--- test_fms/axis_utils/test_axis_utils2.sh
+++ test_fms/axis_utils/test_axis_utils2.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-# Cause test to fail if any command fails.
-set -e
+# This is part of the GFDL FMS package. This is a shell script to
+# execute tests in the test_fms/axis_utils directory.
 
-cp $srcdir/test_fms/axis_utils/input_base.nml input.nml
+# Ed Hartnett 11/26/19
 
+# Set common test settings.
+. ../test_common.sh
+
+# Output for TAP.
+echo 1..1
+
+# Copy and rename namelist file.
+cp $top_srcdir/test_fms/axis_utils/input_base.nml input.nml
+
+# Run the test.
 mpirun -n 2 ./test_axis_utils
+
+# Output for TAP.
+echo ok 1
