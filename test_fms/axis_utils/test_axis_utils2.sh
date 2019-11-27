534098c10f9e5784a858a0bd5e08f88160672955
Edward Hartnett <edward.hartnett@noaa.gov>
2019-11-27 10:04:21 -0700

    took TAP output out of axis_utils

--- test_fms/axis_utils/test_axis_utils2.sh
+++ test_fms/axis_utils/test_axis_utils2.sh
@@ -8,14 +8,8 @@
 # Set common test settings.
 . ../test_common.sh
 
-# Output for TAP.
-echo 1..1
-
 # Copy and rename namelist file.
 cp $top_srcdir/test_fms/axis_utils/input_base.nml input.nml
 
 # Run the test.
 mpirun -n 2 ./test_axis_utils
-
-# Output for TAP.
-echo ok 1
