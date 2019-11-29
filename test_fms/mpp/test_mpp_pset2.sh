c949ee3a281814852dad040d598311c63d29c6a7
Edward Hartnett <edward.hartnett@noaa.gov>
2019-11-29 07:06:04 -0700

    more progress on shell tests

---
+++ test_fms/mpp/test_mpp_pset2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This is part of the GFDL FMS package. This is a shell script to
+# execute tests in the test_fms/mpp directory.
+
+# Ed Hartnett 11/29/19
+
+# Set common test settings.
+. ../test_common.sh
+
+#bats $srcdir/test_fms/mpp/test_mpp_pset.bats
