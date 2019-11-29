0a134fb68e102d1ed88dc4f6cb14ec9fce2aeef2
Edward Hartnett <edward.hartnett@noaa.gov>
2019-11-29 07:43:20 -0700

    fixed shell test script

--- test_fms/mpp_io/test_mpp_io2.sh
+++ test_fms/mpp_io/test_mpp_io2.sh
@@ -11,7 +11,7 @@
 # Copy file for test.
 cp $top_srcdir/test_fms/mpp_io/input_base.nml input.nml
 
-run mpirun -n 1 ./test_mpp_io
+mpirun -n 1 ./test_mpp_io
 
 # This test is skipped in bats file.
 #run mpirun -n 2 ./test_mpp_io
