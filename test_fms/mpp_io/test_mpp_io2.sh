bf0491881f79a824b8415941f7aa8e83f3aabc94
Uriel Ramirez <uramirez8707@gmail.com>
2019-12-10 09:47:02 -0500

    Fix typo in configure.ac, got test_mpp_io to pass on skylake ...

--- test_fms/mpp_io/test_mpp_io2.sh
+++ test_fms/mpp_io/test_mpp_io2.sh
@@ -12,7 +12,23 @@
 cp $top_srcdir/test_fms/mpp_io/input_base.nml input.nml
 
 if test "$mpi_launcher" != "" ; then 
-    npes="-n 1"
+# Get the number of available CPUs on the system
+    if [ $(command -v nproc) ]
+    then
+    # Looks like a linux system
+        nProc=$(nproc)
+    elif [ $(command -v sysctl) ]
+    then
+        # Looks like a Mac OS X system
+        nProc=$(sysctl -n hw.physicalcpu)
+    else
+        nProc=-1
+    fi
+
+    if [ $nProc -gt 12 ]
+    then  
+        npes="-n 12"
+    fi
 fi
 
 $mpi_launcher $npes ./test_mpp_io
