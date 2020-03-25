1bc7818f7ca024a2806b7803bab9e03f9525b8e3
wrongkindofdoctor <Jessica.Liptak@ldt-4359652.gfdl.noaa.gov>
2020-03-25 14:46:46 -0400

    updated authors, fixed directory patah, added lines to remove existing netcdf and log files

--- test_fms/fms2_io/test_fms2_io.sh
+++ test_fms/fms2_io/test_fms2_io.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 # This is part of the GFDL FMS package. This is a shell script to
-# execute tests in the test_fms/field_manager directory.
+# execute tests in the test_fms/fms2_io directory.
 
-# Ed Hartnett 11/29/19
+# Authors: Raymond Menzel
+# Jessica Liptak
 
 # Set common test settings.
 . ../test_common.sh
-
+# Clear out any previous test files
+rm -f *.nc > /dev/null 2>&1
+rm -f *.nc.* > /dev/null 2>&1
+rm -f logfile.*.out > /dev/null 2>&1
 # run the tests
 run_test test_fms2_io 6
