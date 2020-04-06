318de961bd1e71dd07a28d75fec728754242f0cb
Thomas Robinson <Thomas.Robinson@noaa.gov>
2020-04-06 07:27:43 -0400

    Changes handling of namelist variable in fms2_io

--- fms2_io/fms2_io_namelist.F90
+++ fms2_io/fms2_io_namelist.F90
@@ -13,10 +13,12 @@ implicit none
 public :: fms2_io_init
 public :: fms2_ncblksz
 
-integer :: fms2_ncblksz = 1*1024*1024
+integer :: fms2_ncblksz = 1*1024*1024 !< Used as chunksize argument in netcdf file creation calls.
+
 !< Namelist variables
-integer :: ncblksz = 1*1024*1024 !< Space vs time tradeoff: Memory allocated vs number of system calls
-                                 !! for netcdf varaibles  
+integer :: ncblksz = 1*1024*1024 !< User defined chunksize argument in netcdf file creation calls.
+                                 !! Replaces setting the NC_CHKSZ environment variable.
+
 namelist / fms2_io_nml / &
                       ncblksz
 
@@ -29,13 +31,7 @@ integer :: mystat
 
 READ (input_nml_file, NML=fms2_io_nml, IOSTAT=mystat)
 
-print *, ' mystat is ', mystat
- 
-if (mystat == 0) then 
   fms2_ncblksz = ncblksz
-elseif (mystat /= -1) then 
-  call error("fms2_io_init :: There was an error reading the namelist")
-endif
 
 end subroutine fms2_io_init
 
