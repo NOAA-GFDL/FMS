774d51db2df9c526f7dc486e0180a30aeca62499
Thomas Robinson <Thomas.Robinson@noaa.gov>
2020-04-10 14:30:24 -0400

    Adds LGPL to fms2_io_namelist.F90.
    Changes contact email in fms2_io_namelist.F90

--- fms2_io/fms2_io_namelist.F90
+++ fms2_io/fms2_io_namelist.F90
@@ -1,6 +1,24 @@
+!***********************************************************************
+!*                   GNU Lesser General Public License
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* FMS is free software: you can redistribute it and/or modify it under
+!* the terms of the GNU Lesser General Public License as published by
+!* the Free Software Foundation, either version 3 of the License, or (at
+!* your option) any later version.
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+!* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+!* for more details.
+!*
+!* You should have received a copy of the GNU Lesser General Public
+!* License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+!***********************************************************************
 module fms2_io_namelist_mod
 !> @author Tom Robinson
-!> @email thomas.robinson@noaa.gov
+!> @email gfdl.climate.model.info@noaa.gov
 !> @description This module contains the namelist for the fms2_io routines.  The variables are all
 !! module variables, so they can be used elsewhere.  The routine fms2_io_init should be called by
 !! fms_init
