71a9db9931d242bcc8b57d4f85ec1274d8eab005
colingladueNOAA <60889854+colingladueNOAA@users.noreply.github.com>
2020-05-15 18:41:35 -0400

    Update test_read_input_nml2.sh

--- test_fms/mpp/test_read_input_nml2.sh
+++ test_fms/mpp/test_read_input_nml2.sh
@@ -27,13 +27,11 @@
 # Set common test settings.
 . ../test_common.sh
 
-echo "DONE" #TESTING MEASURE
+touch test_numb_base.nml
+echo "&test_read_input_nml_nml" > test_numb_base.nml
+echo "test_numb = 0" >> test_numb_base.nml
+echo "/" >> test_numb_base.nml
 
-#touch test_numb_base.nml
-#echo "&test_read_input_nml_nml" > test_numb_base.nml
-#echo "test_numb = 0" >> test_numb_base.nml
-#echo "/" >> test_numb_base.nml
-#
 ## Test 1
 #sed "s/test_numb = [0-9]/test_numb = 1/" test_numb_base.nml > test_numb.nml
 #cp $top_srcdir/test_fms/mpp/input_base.nml input.nml
