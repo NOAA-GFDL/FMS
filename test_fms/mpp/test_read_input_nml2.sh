befbf95eba0542655de7819e041305ad8011b4a7
colingladueNOAA <60889854+colingladueNOAA@users.noreply.github.com>
2020-05-15 16:03:01 -0400

    Update test_read_input_nml2.sh

--- test_fms/mpp/test_read_input_nml2.sh
+++ test_fms/mpp/test_read_input_nml2.sh
@@ -35,8 +35,7 @@ echo "/" >> test_numb_base.nml
 # Test 1
 sed "s/test_numb = [0-9]/test_numb = 1/" test_numb_base.nml > test_numb.nml
 cp $top_srcdir/test_fms/mpp/input_base.nml input.nml
-run_test test_read_input_nml 1
-#./$top_srcdir/test_fms/mpp/test_read_input_nml.sh
+run_test test_read_input_nml 1 
 if [ $? = 0 ]; then
   echo "Test 1 has passed"
 else
@@ -48,7 +47,6 @@ fi
 sed "s/test_numb = [0-9]/test_numb = 2/" test_numb_base.nml > test_numb.nml
 cp $top_srcdir/test_fms/mpp/input_base.nml input_alternative.nml
 run_test test_read_input_nml 1
-#./$top_srcdir/test_fms/mpp/test_read_input_nml.sh
 if [ $? = 0 ]; then
   echo "Test 2 has passed"
 else
@@ -59,9 +57,9 @@ fi
 # Test 3
 sed "s/test_numb = [0-9]/test_numb = 3/" test_numb_base.nml > test_numb.nml
 cp $top_srcdir/test_fms/mpp/input_base.nml input.nml
-run_test test_read_input_nml 1
-#./$top_srcdir/test_fms/mpp/test_read_input_nml.sh
-if [ $? = 1 ]; then
+err=0
+run_test test_read_input_nml 1 || err=1
+if [ $err = 1 ]; then
   echo "Test 3 has passed"
 else
   echo "ERROR: Test 3 did not hit the error it was expected to"
@@ -73,12 +71,9 @@ sed "s/test_numb = [0-9]/test_numb = 4/" test_numb_base.nml > test_numb.nml
 rm input.nml
 touch input.nml # Achieve a blank namelist to be read
 run_test test_read_input_nml 1
-#./$top_srcdir/test_fms/mpp/test_read_input_nml.sh
 if [ $? = 0 ]; then
   echo "Test 4 has passed"
 else
   echo "ERROR: Test 4 was unsuccessful."
   exit 14
 fi
-
-cp $top_srcdir/test_fms/mpp/input_base.nml input.nml
