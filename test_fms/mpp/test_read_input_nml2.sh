79146c5175ef96a488d6a0ec4b663d29da868553
colingladueNOAA <60889854+colingladueNOAA@users.noreply.github.com>
2020-05-18 15:33:48 -0400

    Update test_read_input_nml2.sh

--- test_fms/mpp/test_read_input_nml2.sh
+++ test_fms/mpp/test_read_input_nml2.sh
@@ -35,7 +35,6 @@ echo "/" >> test_numb_base.nml
 # Test 1
 sed "s/test_numb = [0-9]/test_numb = 1/" test_numb_base.nml > test_numb.nml
 cp $top_srcdir/test_fms/mpp/input_base.nml input.nml
-cat input.nml
 #run_test test_read_input_nml 1
 if [ $? = 0 ]; then
   echo "Test 1 has passed"
@@ -43,10 +42,6 @@ else
   echo "ERROR: Test 1 was unsuccessful."
   exit 11
 fi
-
-cat logfile.000000.out
-cat input.nml
-exit 757
 
 ## Test 2
 #sed "s/test_numb = [0-9]/test_numb = 2/" test_numb_base.nml > test_numb.nml
@@ -75,3 +70,7 @@ else
   echo "ERROR: Test 4 was unsuccessful."
   exit 14
 fi
+
+cat logfile.000000.out
+cat input.nml
+exit 757
