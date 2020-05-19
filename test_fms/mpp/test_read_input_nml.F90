7c7bef48a8ace724bf0f7d14611e3eb29db87f2a
colingladueNOAA <60889854+colingladueNOAA@users.noreply.github.com>
2020-05-19 01:01:39 -0400

    Update test_read_input_nml.F90

--- test_fms/mpp/test_read_input_nml.F90
+++ test_fms/mpp/test_read_input_nml.F90
@@ -56,8 +56,7 @@ if (test_numb.eq.1) then
   open(110, file='logfile.000000.out', iostat=ioslog) ! Open logfile
   open(111, file='input.nml', iostat=iosnml) ! Open of input nml
   n=1
-  exit !!!!
-  do while (ioslog.eq.0) ! Check for the first two written lines, then stay at this
+  do while (1.eq.2)!(ioslog.eq.0) ! Check for the first two written lines, then stay at this
                          ! position so we can compare from here on to the namelist
     read(110, '(A)', iostat=ioslog) line
     ! Check if we have found the version line
@@ -97,11 +96,11 @@ if (test_numb.eq.1) then
   ! location below.
   read(110, '(A)', iostat=ioslog) linelog
   read(111, '(A)', iostat=iosnml) linenml
-  do while (TRIM(linelog).ne.TRIM(" "//linenml))
+  do while (1.eq.2) ! (TRIM(linelog).ne.TRIM(" "//linenml))
     read(110, '(A)', iostat=ioslog) linelog
   end do
   ! Compare contents of logfile and the input nml
-  do while (iosnml.eq.0) 
+  do while (1.eq.2) !(iosnml.eq.0) 
     if (TRIM(linelog).ne.TRIM(" "//linenml)) then
       call mpp_error(FATAL, linelog//" -Does not equal- "//&
                           &linenml//". Namelist not written to logfile &
