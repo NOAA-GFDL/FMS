4a90e254030e82045b2271d975d34dabac0eb709
colingladueNOAA <60889854+colingladueNOAA@users.noreply.github.com>
2020-05-18 21:38:07 -0400

    Update test_read_input_nml2.sh

--- test_fms/mpp/test_read_input_nml2.sh
+++ test_fms/mpp/test_read_input_nml2.sh
@@ -35,6 +35,7 @@ echo "/" >> test_numb_base.nml
 # Test 1
 sed "s/test_numb = [0-9]/test_numb = 1/" test_numb_base.nml > test_numb.nml
 cp $top_srcdir/test_fms/mpp/input_base.nml input.nml
+cat input.nml
 run_test test_read_input_nml 1
 if [ $? = 0 ]; then
   echo "Test 1 has passed"
