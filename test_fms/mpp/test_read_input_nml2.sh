05bb84e17f299bad4b4f62b78ecffd4cb39e2e25
colingladueNOAA <60889854+colingladueNOAA@users.noreply.github.com>
2020-05-19 19:48:17 -0400

    Update test_read_input_nml2.sh

--- test_fms/mpp/test_read_input_nml2.sh
+++ test_fms/mpp/test_read_input_nml2.sh
@@ -43,7 +43,7 @@ if [ $? = 0 ]; then
   echo "000000000000000000"
   cat log.txt
   echo "111111111111111111"
-  comm -12 log.txt inp.txt > shar.txt
+  comm -12 inp.txt log.txt > shar.txt
   cat shar.txt
   echo "222222222222222222"
   exit 1
