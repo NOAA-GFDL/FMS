a91d6b96371b0d7ea581ea80c3372b2d4c9ff463
Edward Hartnett <Edward.Hartnett@noaa.gov>
2020-06-23 11:02:09 -0600

    simple test for mpp_domains

---
+++ test_fms/mpp/test_domains_simple.F90
@@ -0,0 +1,49 @@
+!***********************************************************************
+!*                   GNU Lesser General Public License
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* FMS is free software: you can redistribute it and/or modify it under
+!* the terms of the GNU Lesser General Public License as published by
+!* the Free Software Foundation, either version 3 of the License, or (at
+!* your option) any later version.
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+!* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+!* for more details.
+!*
+!* You should have received a copy of the GNU Lesser General Public
+!* License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+!***********************************************************************
+program test_domains_simple
+  use mpp_mod
+  use mpp_domains_mod
+
+  implicit none
+#include "../../include/fms_platform.h"
+  integer :: pe, npes
+  integer :: nx=128, ny=128, nz=40, stackmax=4000000
+  integer :: layout(2)
+  type(domain2D) :: domain
+  
+  call mpp_init()
+
+  pe = mpp_pe()
+  npes = mpp_npes()
+
+  !--- initialize mpp domains
+  call mpp_domains_init(MPP_DEBUG)
+  call mpp_domains_set_stack_size(stackmax)
+
+  call mpp_define_layout( (/1,nx,1,ny/), npes, layout )
+  if (npes .eq. 4) then
+     if (layout(1) .ne. 2 .or. layout(2) .ne. 2) call mpp_error(FATAL, 'bad layout values')
+  endif
+  
+  call mpp_define_domains( (/1,nx,1,ny/), layout, domain )
+  
+  call mpp_domains_exit()
+  call mpp_exit()
+
+end program test_domains_simple
