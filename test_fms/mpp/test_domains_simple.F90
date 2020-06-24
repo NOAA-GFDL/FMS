7598271ad032b13c64462b939a2751404ab32f72
Edward Hartnett <Edward.Hartnett@noaa.gov>
2020-06-24 11:29:27 -0600

    now check some domain values

--- test_fms/mpp/test_domains_simple.F90
+++ test_fms/mpp/test_domains_simple.F90
@@ -30,10 +30,11 @@ program test_domains_simple
   implicit none
 #include "../../include/fms_platform.h"
   integer :: pe, npes
-  integer :: nx=128, ny=128, nz=40
+  integer :: nx=40, ny=40, nz=40
   integer :: layout(2)
   type(domain2D) :: domain_2D
   type(domain1D) :: domain_1D
+  integer :: is, ie, js, je
   
   call mpp_init()
 
@@ -54,9 +55,32 @@ program test_domains_simple
   ! Define a 1D domain.
   !call mpp_define_domains((/1, nx/), 4, domain_1D)
   call mpp_define_domains((/1, nx/), 4, domain_1D, pelist=(/0, 1, 2, 3/))
+  call mpp_get_compute_domain(domain_1D, is, ie)
+  
+  ! Check results when run on 4 pes.
+  if (npes .eq. 4) then
+     if (is .ne. pe * 10 + 1) call mpp_error(FATAL, 'bad domain start value')
+     if (ie .ne. 10 * (pe + 1)) call mpp_error(FATAL, 'bad domain end value')
+  endif
 
   ! Define a 2D domain.
   call mpp_define_domains((/1, nx, 1, ny/), layout, domain_2D)
+  call mpp_get_compute_domain(domain_2D, xbegin = is, xend = ie, ybegin = js, yend = je)
+  print *, pe, is, ie, js, je
+
+  ! Check results when run on 4 pes.
+  if (npes .eq. 4) then
+     if (pe .eq. 0 .or. pe .eq. 2) then
+        if (is .ne. 1 .or. ie .ne. 20) call mpp_error(FATAL, 'bad 2D domain')
+     else
+       if (is .ne. 21 .or. ie .ne. 40) call mpp_error(FATAL, 'bad 2D domain')
+     endif
+     if (pe .eq. 0 .or. pe .eq. 1) then
+        if (js .ne. 1 .or. je .ne. 20) call mpp_error(FATAL, 'bad 2D domain')
+     else
+        if (js .ne. 21 .or. je .ne. 40) call mpp_error(FATAL, 'bad 2D domain')
+     endif
+  endif
 
   ! Clean up domains.
   call mpp_domains_exit()
