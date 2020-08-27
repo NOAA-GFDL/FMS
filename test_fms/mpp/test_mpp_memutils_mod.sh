7dd7567a229b43c0c7b836bae18e0d77601af388
wrongkindofdoctor <Jessica.Liptak@lscsky50-d.gfdl.noaa.gov>
2020-08-27 15:30:41 -0400

    changed mpirun to run_test in test_mpp_memutils_mod.sh

--- test_fms/mpp/test_mpp_memutils_mod.sh
+++ test_fms/mpp/test_mpp_memutils_mod.sh
@@ -30,16 +30,16 @@
 touch input.nml
 
 echo "1: Test begin/end routines of mpp_memutils_mod"
-mpirun -n 1 ./test_mpp_memutils_begin_end
+run_test -n 1 ./test_mpp_memutils_begin_end
 
 echo "2: Test mpp_print_memuse_stats"
-mpirun -n 1 ./test_mpp_print_memuse_stats_stderr
+run_test -n 1 ./test_mpp_print_memuse_stats_stderr
 
 echo "3: Test mpp_print_memuse_stats to file (stdout)"
-mpirun -n 1 ./test_mpp_print_memuse_stats_file
+run_test -n 1 ./test_mpp_print_memuse_stats_file
 
 echo "4: Test failure caught if mpp_memuse_begin called multiple times"
-mpirun -n 1 ./test_mpp_memutils_begin_2x || echo "Ok"
+run_test -n 1 ./test_mpp_memutils_begin_2x || echo "Ok"
 
 echo "5: Test failure caught if mpp_memuse_end called before mpp_memuse_begin"
-mpirun -n 1 ./test_mpp_memutils_end_before_begin || echo "Ok"
+run_test -n 1 ./test_mpp_memutils_end_before_begin || echo "Ok"
