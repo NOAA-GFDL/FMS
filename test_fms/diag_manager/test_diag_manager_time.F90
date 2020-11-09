a900c53da2833424d5f88716519cb4e141d3ba90
Uriel Ramirez <uramirez8707@gmail.com>
2020-11-09 14:29:19 -0500

    Adds tests to test_fms/diag_manager

--- test_fms/diag_manager/test_diag_manager_time.F90
+++ test_fms/diag_manager/test_diag_manager_time.F90
@@ -1,4 +1,29 @@
-program test_diag_manager
+!***********************************************************************
+!*                   GNU Lesser General Public License
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* FMS is free software: you can redistribute it and/or modify it under
+!* the terms of the GNU Lesser General Public License as published by
+!* the Free Software Foundation, either version 3 of the License, or (at
+!* your option) any later version.
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+!* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+!* for more details.
+!*
+!* You should have received a copy of the GNU Lesser General Public
+!* License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+!***********************************************************************
+
+!> @brief  This programs tests diat_manager with the following diag_table
+!! test_diag_manager
+!! 2 1 1 0 0 0
+!! "ocn%4yr%2mo%2dy%2hr",      1,  "days", 1, "days", "time", 1, "days", "2 1 1 0 0 0"
+!! "test_diag_manager_mod", "sst", "sst", "ocn%4yr%2mo%2dy%2hr",  "all", .true., "none", 2
+
+program test_diag_manager_time
 
 use   mpp_domains_mod
 use   diag_manager_mod
@@ -71,4 +96,4 @@ enddo
 call diag_manager_end(Time)
 call fms_end
 
-end program test_diag_manager
+end program test_diag_manager_time
