f6a9e12beb53b343159c9d6a863be8964e47d5cc
Seth Underwood <Seth.Underwood@noaa.gov>
2020-12-30 14:59:03 -0500

    Remove possible double MOD_OUTPUT options
    GFortran does not like multiple -J options.  Updated mkmods.mk to remove the possible double -J

--- mkmods.mk
+++ mkmods.mk
@@ -3,7 +3,7 @@ $(shell test -d $(MODDIR) || mkdir -p $(MODDIR))
 
 SUFFIXES = .$(FC_MODEXT) _mod.$(FC_MODEXT)
 .F90.$(FC_MODEXT) .F90_mod.$(FC_MODEXT) .f90.$(FC_MODEXT) .f90_mod.$(FC_MODEXT):
-	$(PPFCCOMPILE) $(FC_MODOUT)$(MODDIR) -c $<
+	$(PPFCCOMPILE) -c $<
 	@cp $(MODDIR)/$@ .
 
 CLEANFILES = *.$(FC_MODEXT) $(BUILT_SOURCES:%=$(MODDIR)/%) *__genmod.$(FC_MODEXT) *__genmod.f90
