a4d31abd2e95783bf363a488456ed1522a156f31
Uriel Ramirez <uramirez8707@gmail.com>
2021-07-06 13:28:28 -0400

    Corrects minor typos in test_fms/fms2_io/test_global_att.F90 to make comments consistent

--- test_fms/fms2_io/test_global_att.F90
+++ test_fms/fms2_io/test_global_att.F90
@@ -24,19 +24,19 @@ use   fms2_io_mod
 use   mpp_mod
 use   platform_mod
 
-type(FmsNetcdfFile_t) :: fileobj  !< fms2io netcd file obj
-real(kind=r8_kind) :: buf_r8_kind !< r8_kind buffer
-real(kind=r8_kind), dimension(2) :: buf_r8_kind_1d !< r8_kind 1D buffer
-real(kind=r4_kind) :: buf_r4_kind !< r4_kind buffer
-real(kind=r4_kind), dimension(2)  :: buf_r4_kind_1d !< r4_kind 1D buffer
-integer(kind=i4_kind)   :: buf_i4_kind !< i4_kind buffer
-integer(kind=i4_kind), dimension(2)    :: buf_i4_kind_1d !< i4_kind 1D buffer
-integer(kind=i8_kind)   :: buf_i8_kind !< i8_kind buffer
-integer(kind=i8_kind), dimension(2)    :: buf_i8_kind_1d !< i8_kind 1D buffer
+type(FmsNetcdfFile_t) :: fileobj  !> fms2io netcd file obj
+real(kind=r8_kind) :: buf_r8_kind !> r8_kind buffer
+real(kind=r8_kind), dimension(2) :: buf_r8_kind_1d !> r8_kind 1D buffer
+real(kind=r4_kind) :: buf_r4_kind !> r4_kind buffer
+real(kind=r4_kind), dimension(2)  :: buf_r4_kind_1d !> r4_kind 1D buffer
+integer(kind=i4_kind)   :: buf_i4_kind !> i4_kind buffer
+integer(kind=i4_kind), dimension(2)    :: buf_i4_kind_1d !> i4_kind 1D buffer
+integer(kind=i8_kind)   :: buf_i8_kind !> i8_kind buffer
+integer(kind=i8_kind), dimension(2)    :: buf_i8_kind_1d !> i8_kind 1D buffer
 character (len = 120), dimension(3) :: my_format !> Array of formats to try.
 
-character(len=20) :: buf_str !< character buffer
-integer :: i !< For Do loop
+character(len=20) :: buf_str !> character buffer
+integer :: i !> For Do loop
 
 call fms2_io_init
 call mpp_init
@@ -57,7 +57,7 @@ if (open_file(fileobj, "test_global_att_"//trim(my_format(i))//".nc", "overwrite
    call register_global_attribute(fileobj, "buf_i4_kind", int(3, kind=i4_kind))
    call register_global_attribute(fileobj, "buf_i4_kind_1d", (/ int(3, kind=i4_kind), int(5, kind=i4_kind) /) )
 
-   !< int8 is only supported with the "netcdf4" type
+   !> int8 is only supported with the "netcdf4" type
    if(i .eq. 3) then
      call register_global_attribute(fileobj, "buf_i8_kind", int(2, kind=i8_kind))
      call register_global_attribute(fileobj, "buf_i8_kind_1d", (/ int(2, kind=i8_kind), int(4, kind=i8_kind) /) )
@@ -81,7 +81,7 @@ if (open_file(fileobj, "test_global_att_"//trim(my_format(i))//".nc", "read", nc
    call get_global_attribute(fileobj, "buf_i4_kind", buf_i4_kind)
    call get_global_attribute(fileobj, "buf_i4_kind_1d", buf_i4_kind_1d)
 
-   !< int8 is only supported with the "netcdf4" type
+   !> int8 is only supported with the "netcdf4" type
    if(i .eq. 3) then
      call get_global_attribute(fileobj, "buf_i8_kind", buf_i8_kind)
      call get_global_attribute(fileobj, "buf_i8_kind_1d", buf_i8_kind_1d)
@@ -107,7 +107,7 @@ if (buf_i4_kind /= int(3, kind=i4_kind)) call mpp_error(FATAL, "test_global_att-
 if (buf_i4_kind_1d(1) /= int(3, kind=i4_kind) .or. buf_i4_kind_1d(2) /= int(5, kind=i4_kind)) &
     call mpp_error(FATAL, "test_global_att-"//trim(my_format(i))//": error reading buf_i4_kind_1d")
 
-!< int8 is only supported with the "netcdf4" type
+!> int8 is only supported with the "netcdf4" type
 if(i .eq. 3) then
    if (buf_i8_kind /= int(2, kind=i8_kind)) call mpp_error(FATAL, "test_global_att-"//trim(my_format(i))//": error reading buf_i8_kind")
    if (buf_i8_kind_1d(1) /= int(2, kind=i8_kind) .or. buf_i8_kind_1d(2) /= int(4, kind=i8_kind)) &
@@ -119,7 +119,7 @@ if (trim(buf_str) /= "some text") then
     call mpp_error(FATAL, "test_global_att-"//trim(my_format(i))//": error reading buf_str")
 endif
 
-enddo !< do i = 1, size(my_format)
+enddo !> do i = 1, size(my_format)
 
 call mpp_exit()
 end program
