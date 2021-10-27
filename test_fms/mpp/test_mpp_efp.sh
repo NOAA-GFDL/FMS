b87e04c97f10aba36ed41e2ab73ea9464fdbaeab
rem1776 <Ryan.Mulhall@lscamd50-d.gfdl.noaa.gov>
2021-10-27 16:39:43 -0400

    Add header, fix makefile/ci

--- test_fms/mpp/test_mpp_efp.sh
+++ test_fms/mpp/test_mpp_efp.sh
@@ -22,10 +22,13 @@
 # This is part of the GFDL FMS package. This is a shell script to
 # execute tests in the test_fms/mpp directory.
 
-# Tom Robinson 04/21/2020
+# Ryan Mulhall 10/27/21 
 
 # Set common test settings.
 . ../test_common.sh
+
+# makes directory to avoid other tests overwriting input.nml
+mkdir test_mpp_efp_output && cd test_mpp_efp_output
 
 cat <<_EOF > input.nml
 &test_mpp_efp_nml
@@ -33,8 +36,10 @@ test_num = 1
 /
 _EOF
 
-run_test test_mpp_efp 4
+run_test ../test_mpp_efp 4
 sed -i 's/test_num = 1/test_num = 2/' input.nml
-run_test test_mpp_efp 4
+run_test ../test_mpp_efp 4
 sed -i 's/test_num = 2/test_num = 3/' input.nml
-run_test test_mpp_efp 4
+run_test ../test_mpp_efp 4
+
+cd .. && rm -rf test_mpp_efp_output
