1e6cb523f351d75d3f9e0cffb5780dd739a05f3f
Miguel R Zuniga <42479054+ngs333@users.noreply.github.com>
2022-02-07 13:37:31 -0500

    diag_manager ontainer and linked list -test core dump fixes and more (#888)
    * Initial commit of the fms_diag_object_container. Includes the underlying
    linked_list library, some changes to diag_manger to initialize the container
    and to use the container upon field registration, related Makefile.am changes.
    
    * Modified diag object iterator to fix casting compilation error on CI system.
    
    * Initial modificationss in response to a review by Tom Robinsom
    on  12/7/01. Mostly documentation, logging, and type name improvements.
    
    * Experimenting with documentation annotations.
    
    * Added test of fms_diag_object_container class. Further changes to follow convention
    and documentation.
    
    * Corrected script calling unit test. Added todo in fms_diag_yaml.F90.
    
    * Cleaned up test of container.
    
    * Addded a "TODO:" to fms_diag_yaml.F90, function is filed type null.
    
    * Modified new files and unit test for further compliance with coding standards
    
    * Renamed the linked list mod. Corrected CMakeList.txtx and a Makefile.am.
    
    * Renamed linked list mod.  Changed a Makefile.am.
    
    * Fixed CMakeLists.txt.
    
    * Mods to CMakeLIst.txt. Includes adding parser dir files.
    
    * Fixing typo in CMakeLists.txt
    
    * Many comments and documentation chages based on Tom R's 2nd review.
    
    * Adds annotations @addtogroup and @{ to the diag object container mod and the linked list mod.
    
    * Added comments for the "this" variable. Nods to use "!<" for var comments.
    
    * Further doxygen related improvements. Some improvements on on calss access (private/public) lables.
    
    * Five chages of list node instance declarations from class to node to
    compile on Intel.
    
    * One change from type to class for an interator instance. Some comment updates.
    
    * Added test_diag_dlinked_list.F90. Modified memeber data/access in several
    in several types. Improved several comments.
    
    * Improved or added several comments.
    
    * Removed vs code related files.
    
    * Removing extraneous return statement in test_diag_dlinked_list.F90.
    
    * Improved test_diag_dlinked_list.F90, both code and comments.
    
    * Removed duplicate yaml_parser.F90 from CMakeLists.txt.
    
    * Incorporates changes based on Tom R's latest review.
    
    * Includes some doxygen related changes requested by Ryan M.
    
    * Skipping linked list and the container tests by adding skip arg to calls in
    test_diaf_manager2.sh
    
    * Fixes the core dump of test_diag_dlinked_list. Also does some
    cleanup of the linked list and container classes, and their unit
    tests.
    
    * Improved comments, especially destructor and clear function.
    
    * Modified diag_manager to the containers initialize routine.
    
    * Post review making sure member vars are private.
    
    * Renamed linked list node member "data" to "data_ptr".

--- test_fms/diag_manager/test_diag_dlinked_list.F90
+++ test_fms/diag_manager/test_diag_dlinked_list.F90
@@ -33,10 +33,9 @@
 !! class in the book ``Data Structures And Algorithm Analysis in C++",
 !! 3rd Edition, by Mark Allen Weiss.
 program test_diag_dlinked_list
-   use mpp_mod, only: mpp_init, mpp_exit, mpp_error, FATAL, WARNING
-   use mpp_mod, only : mpp_set_stack_size, mpp_init_test_requests_allocated
+   use mpp_mod, only: mpp_init, mpp_set_stack_size, mpp_init_test_requests_allocated
    use mpp_io_mod, only: mpp_io_init
-
+   use fms_mod, ONLY: error_mesg, FATAL,NOTE
    use fms_diag_object_mod, only : fmsDiagObject_type
    use fms_diag_dlinked_list_mod, only : FmsDlList_t, FmsDllIterator_t
 
@@ -45,7 +44,7 @@ program test_diag_dlinked_list
    !> @brief  This class is the type for the data to insert in the linked list.
    type TestDummy_t
       integer :: id  = 0
-      character(len=20) :: name
+      real    :: weight = 1000
    end type TestDummy_t
 
    !!
@@ -61,27 +60,27 @@ program test_diag_dlinked_list
    logical :: test_passed                       !< Flag indicating if the test_passed
    !! These fields below used to initialize diag object data. TBD
    integer :: id
-   character(:), allocatable :: mname, mname_pre
    !!
-
-
-   test_passed = .true.  !! will be set to false if there are any issues.
 
    call mpp_init(mpp_init_test_requests_allocated)
    call mpp_io_init()
    call mpp_set_stack_size(145746)
 
+   call error_mesg("test_diag_linked_list", "Starting tests",NOTE)
+
+   test_passed = .true.  !! will be set to false if there are any issues.
+
    !! Ids will initially be from 1 to num_objs, so :
    full_id_sum = (num_objs * (num_objs + 1)) / 2
 
-   !!Create the list
-   list = FmsDlList_t()
+   !! Create the list
+   allocate(list)
+   call list%initialize()
 
    if( list%size() /= 0) then
-      test_passed = .false.
-      call mpp_error(FATAL, "list incorrect size. Expected 0 at start")
+     test_passed = .false.
+     call error_mesg("test_diag_linked_list", "list incorrect size. Expected 0 at start",FATAL)
    endif
-   mname_pre = "ATM"
 
   !! Initialize num_objs objects and insert into list one at a time.
   !! The loop iterator is same as id - created in order to facilitate
@@ -90,9 +89,8 @@ program test_diag_dlinked_list
       !!Allocate on heap another test dummy object :
       allocate (p_td_obj)
       !! And set some of its dummy data :
-      call combine_str_int(mname_pre, id, mname)
       p_td_obj%id = id
-      p_td_obj%name = mname
+      p_td_obj%weight = id + 1000
       !! And have the "Char(*) pointer also point to it:
       p_obj => p_td_obj
 
@@ -100,14 +98,14 @@ program test_diag_dlinked_list
       iter = list%push_back( p_obj)
       if(iter%has_data() .eqv. .false. ) then
          test_passed = .false.
-         call mpp_error(FATAL, "List push_back error.")
+         call error_mesg("test_diag_dlinked_list", "List push_back error.",FATAL)
       endif
 
    enddo
 
    if( list%size() /= num_objs) then
       test_passed = .false.
-      call mpp_error(FATAL, "List has incorrect size after inserts.")
+      call error_mesg("test_diag_dlinked_list", "List has incorrect size after inserts.",FATAL)
    endif
 
 
@@ -117,24 +115,27 @@ program test_diag_dlinked_list
 
    if( sum  /=  full_id_sum) then
       test_passed = .false.
-      call mpp_error(FATAL, "Id sums via iteration over the list objects is not as expected")
+      call error_mesg("test_diag_dlinked_list", &
+        &"Id sums via iteration over the list objects is not as expected",FATAL)
    endif
 
    if( list%size() /= num_objs) then
       test_passed = .false.
-      call mpp_error(FATAL, "The list size is not as expected post inserts.")
+      call error_mesg("test_diag_dlinked_list", &
+        &"The list size is not as expected post inserts.",FATAL)
    endif
 
    !! Test a removal from the back (id should be num_objs)
    p_obj => find_back_of_list( list)
-   iter = list%pop_back()
+   iter  = list%pop_back()
    !! Note the client is resposible for managing memory of anything he explicitly
    !! removes from the list:
    deallocate(p_obj)
    sum = sum_ids_in_list ( list )
    if( sum  /=  full_id_sum - num_objs ) then
       test_passed = .false.
-      call mpp_error(FATAL, "Id sums via iteration over the list objects is not as expected")
+      call error_mesg("test_diag_dlinked_list", &
+        &"Id sums via iteration over the list objects is not as expected",FATAL)
    endif
 
    !! Repeat - test removal from the back of list (should be (num_objs -1)).
@@ -146,93 +147,81 @@ program test_diag_dlinked_list
    sum = sum_ids_in_list ( list )
    if( sum  /=  (full_id_sum - num_objs - (num_objs -1) )) then
       test_passed = .false.
-      call mpp_error(FATAL, "Id sums via iteration over the list objects is not as expected")
+      call error_mesg("test_diag_dlinked_list", &
+        & "Id sums via iteration over the list objects is not as expected",FATAL)
    endif
 
+   !! List.clear() is called by the destructor automatically, but for further testing
+   !! we will use it to renove (and deallocate) the data nodes and associated data
+   !! of the list.
    call list%clear()
    if( list%size() /= 0) then
       test_passed = .false.
-      call mpp_error(FATAL, "List is incorrect size after clearing.")
+      call error_mesg("test_diag_dlinked_list", &
+        "List is incorrect size after clearing.",FATAL)
    endif
 
-   write (6,*) "Finishing diag_dlinked_list tests."
-
-   !! the list has a finalize/destructor which will deallocate data that is still it list.
-   !! equivalent to calling list%clear() as above.
+   !! Allocated objects are deallocated automatically, but one can aslo make the call.
    deallocate(list)
+
+   call error_mesg('test_diag_dlinked_list', 'Test has finished',NOTE)
 
    call MPI_finalize(ierr)
 
 CONTAINS
 
-
-
    !> @brief Cast the "class(*) input data to the expected type.
-   function  get_typed_data( data_in  ) result( rdo )
-      class(*), intent(in), pointer :: data_in !< An input pointer to the class(*) object.
-      class(TestDummy_t),  pointer :: rdo !< The resultant pointer to the expected underlying object type.
-      rdo => null()
-
-      select type(data_in)
+   function  get_typed_data( pci ) result( pdo )
+      class(*), intent(in), pointer :: pci !< An input pointer to the class(*) data object.
+      class(TestDummy_t),  pointer :: pdo !< The resultant pointer to the expected underlying object type.
+      !
+      pdo => null()
+      select type(pci)
        type is (TestDummy_t)  !! "type is", not the (polymorphic) "class is"
-         rdo => data_in
+         pdo => pci
        class default
-         call mpp_error(FATAL, "Data to access is not of expected type.",FATAL)
+         call error_mesg("test_diag_dlinked_list", &
+           & "Data to access is not of expected type.",FATAL)
       end select
    end function get_typed_data
 
    !> Calcualte the sum of the ids.
    !! Exercises iteration over the list.
-   function sum_ids_in_list (list) result (rsum)
-      type (FmsDlList_t), allocatable :: list !< The linked list instance
+   function sum_ids_in_list (the_list) result (rsum)
+      type (FmsDlList_t), intent(inout) , allocatable :: the_list !< The linked list instance
       integer  :: rsum                        !< The resultant sum of ids
       class(FmsDllIterator_t), allocatable :: iter !< An iterator over the list
-      type (TestDummy_t), pointer::   p_td_obj     !< A pointer to a test_dummy object
-      class(*), pointer :: p_obj                   !< A pointer to a class(*) object
-      integer :: ic_status                         !< A list insertion status.
+      type (TestDummy_t), pointer:: p_td_obj => null() !< A pointer to a test_dummy object
+      class(*), pointer :: p_obj  => null()            !< A pointer to a class(*) object
+      integer :: ic_status                             !< A list insertion status.
       !!
       rsum = 0
-      iter = list%get_literator()
+      iter = the_list%get_literator()
       do while( iter%has_data() .eqv. .true.)
          p_obj => iter%get()
          p_td_obj => get_typed_data (p_obj )
-         id =  p_td_obj%id
-         rsum = rsum + id
+         rsum = rsum + p_td_obj%id
          ic_status = iter%next()
       end do
    end function sum_ids_in_list
 
-  !> Calcualate the sum of the ids. This also is a kind of search function,
+  !> Find the past object in list. This also is a kind of search function,
    !! so if the provided wrapper is not used, you have to write your own.
    !! @return a pointer the object at the end of the list, or null if none
-   function find_back_of_list (list) result (p_rdo)
-    type (FmsDlList_t), allocatable :: list !< The linked list instance
-    class(TestDummy_t),  pointer :: p_rdo !< The resultant back of list,
+   function find_back_of_list (the_list) result (pdo)
+    type (FmsDlList_t), intent(inout) , allocatable ::the_list  !< The linked list instance
+    class(TestDummy_t),  pointer :: pdo !< The resultant back of list,
     class(FmsDllIterator_t), allocatable :: iter !< An iterator over the list
-    class(*), pointer :: p_obj                   !< A pointer to a class(*) object
+    class(*), pointer :: p_obj => null()         !< A pointer to a class(*) object
     integer :: ic_status                         !< A list insertion status.
     !!
-    p_rdo => null()
-    iter = list%get_literator()
+    pdo=>null()
+    iter = the_list%get_literator()
     do while( iter%has_data() .eqv. .true.)
        p_obj => iter%get()
-       p_rdo => get_typed_data (p_obj )
+       pdo => get_typed_data (p_obj )
        ic_status = iter%next()
     end do
  end function find_back_of_list
 
- subroutine combine_str_int (str, num, rs)
-    character(:), allocatable, intent (in):: str
-    integer ,    intent (in) :: num
-    character(:), allocatable, intent (out) :: rs
-    character(len_trim(str) + 8) :: tmp
-
-    write (tmp, "(A4,I4)") str,num
-    tmp = trim(tmp)
-    rs = tmp
- end subroutine combine_str_int
-
-
 end program test_diag_dlinked_list
-
-
