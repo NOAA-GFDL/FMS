7e1bb4e449467edf59fc2db1669bc2f3a0482fa8
Caitlyn Mcallister <caitlyn.mcallister@noaa.gov>
2023-03-06 14:40:01 -0500

    Removes unecessary precision

--- axis_utils/axis_utils2.F90
+++ axis_utils/axis_utils2.F90
@@ -40,7 +40,7 @@ module axis_utils2_mod
   private
 
   integer, parameter :: maxatts = 100
-  real(r8_kind), parameter    :: epsln = real(1.e-10, kind=r8_kind)
+  real(r8_kind), parameter    :: epsln = 1.e-10_r8_kind
   real(r8_kind), parameter    :: fp5 = 0.5_r8_kind, f360 = 360.0_r8_kind
 
 !> @}
