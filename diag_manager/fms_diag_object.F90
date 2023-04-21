3b3aa38f68f8bf22447b356f74c7b6c236ca6a34
ganganoaa <121043264+ganganoaa@users.noreply.github.com>
2023-04-21 16:28:45 -0400

    Update fms_diag_object.F90

--- diag_manager/fms_diag_object.F90
+++ diag_manager/fms_diag_object.F90
@@ -881,9 +881,9 @@ subroutine allocate_diag_field_output_buffers(this, field_data, field_id)
   class(fmsDiagOutputBuffer_class), pointer :: ptr_diag_buffer_obj !< Pointer to the buffer class
   class(DiagYamlFilesVar_type), pointer :: ptr_diag_field_yaml !< Pointer to a field from yaml fields
   integer, pointer :: axis_ids(:) !< Pointer to indices of axes of the field variable
-  class(*), allocatable :: var_type !< Stores type of the field data (r4, r8, i4, i8, and string) represented as an integer.
-  class(*), allocatable :: missing_value !< Fill value to initialize output buffers
-  class(*), allocatable :: var_name !< Field name to initialize output buffers
+  integer :: var_type !< Stores type of the field data (r4, r8, i4, i8, and string) represented as an integer.
+  real :: missing_value !< Fill value to initialize output buffers
+  character(*), allocatable :: var_name !< Field name to initialize output buffers
 
   ! Determine the type of the field data
   var_type = get_var_type(field_data(1, 1, 1, 1))
@@ -894,8 +894,23 @@ subroutine allocate_diag_field_output_buffers(this, field_data, field_id)
   ! Get missing value for the field
   if (this%FMS_diag_fields(field_id)%has_missing_value()) then
     missing_value = this%FMS_diag_fields(field_id)%get_missing_value(var_type)
+    select type (my_type => this%FMS_diag_fields(field_id)%get_missing_value(var_type))
+      type is (real(kind=r4_kind))
+        missing_value = my_type
+      type is (real(kind=r8_kind))
+        missing_value = real(my_type)
+      class default
+        call mpp_error( FATAL, 'fms_diag_object_mod:allocate_diag_field_output_buffers Invalid type')
+    end select
   else
-    missing_value = get_default_missing_value(var_type)
+    select type (my_type => get_default_missing_value(var_type))
+      type is (real(kind=r4_kind))
+        missing_value = my_type
+      type is (real(kind=r8_kind))
+        missing_value = real(my_type)
+      class default
+        call mpp_error( FATAL, 'fms_diag_object_mod:allocate_diag_field_output_buffers Invalid type')
+    end select
   endif
 
   ! Determine dimensions of the field
