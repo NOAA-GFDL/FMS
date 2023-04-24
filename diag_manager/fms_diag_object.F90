17e0c56500f2bbf870917f98cb17a6f18fe8cfb6
ganganoaa <121043264+ganganoaa@users.noreply.github.com>
2023-04-24 12:53:36 -0400

    Update fms_diag_object.F90

--- diag_manager/fms_diag_object.F90
+++ diag_manager/fms_diag_object.F90
@@ -903,10 +903,7 @@ subroutine allocate_diag_field_output_buffers(this, field_data, field_id)
   if (this%FMS_diag_fields(field_id)%has_missing_value()) then
     select type (my_type => this%FMS_diag_fields(field_id)%get_missing_value(var_type))
       type is (real(kind=r4_kind))
-        select type (missing_value)
-          type is (real(kind=r4_kind))
-            missing_value = real(my_type, kind=r4_kind)
-        end select
+        missing_value = my_type
       type is (real(kind=r8_kind))
         missing_value = real(my_type)
       class default
