b55a56a5fed2c780499459b2c13acbdc7360392d
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2023-04-28 09:14:32 -0400

    add tests

---
+++ test_fms/tracer_manager/test_tracer_manager.F90
@@ -0,0 +1,87 @@
+program test_tracer_manager
+
+  use fms_mod, only: fms_init
+  use mpp_mod, only: mpp_error, FATAL
+  use field_manager_mod, only: field_manager_init, MODEL_ATMOS, MODEL_OCEAN, MODEL_LAND, fm_dump_list, fm_get_value
+  use tracer_manager_mod
+  use platform_mod, only: r4_kind, r8_kind
+
+  implicit none
+
+  call test_set_tracer_profile
+
+contains
+
+  subroutine test_set_tracer_profile
+  integer, parameter :: numlevels=10
+  integer, parameter :: npoints=5
+
+  integer :: tracer_index, success, i, j, k
+  real :: top_value, bottom_value, surf_value, multiplier
+  real :: tracer_out1(1,1,1), tracer_out2(npoints,npoints,numlevels)
+  real :: answer1(1,1,1), answer2(npoints,npoints,numlevels)
+
+  character(128) :: err_message
+
+  call fms_init
+  call tracer_manager_init
+
+  !-- profile_type=fixed --!
+
+  !> the tracer 'radon' profile type is 'fixed' (see field_table)
+  !> the tracer field value should be zero.
+  tracer_index=get_tracer_index(MODEL_ATMOS, 'radon')
+  call set_tracer_profile(MODEL_ATMOS, tracer_index, tracer_out1,err_message)
+  !> answer
+  answer1(1,1,1)=0.0
+  !> check results
+  if(tracer_out1(1,1,1).ne.answer1(1,1,1)) call mpp_error(FATAL,'ATM tracer field value should be 0.0')
+
+
+  !-- OCEAN profile_type=profile --!
+
+  tracer_index=get_tracer_index(MODEL_OCEAN, 'immadeup2')
+  call set_tracer_profile(MODEL_OCEAN,tracer_index,tracer_out2,err_message)
+  !> answer
+  success=fm_get_value("/ocean_mod/tracer/immadeup2/profile_type/profile/bottom_value", bottom_value)
+  success=fm_get_value("/ocean_mod/tracer/immadeup2/profile_type/profile/surface_value", surf_value)
+  multiplier = exp( log (bottom_value/surf_value) /real(numlevels-1,r8_kind))
+  answer2(:,:,numlevels)=surf_value
+  do i=numlevels-1, 1, -1
+     answer2(:,:,i) = answer2(:,:,i+1)*multiplier
+  end do
+  !> check results
+  do k=1, numlevels
+     do j=1, npoints
+        do i=1, npoints
+           if( tracer_out2(i,j,k) .ne. answer2(i,j,k)) &
+                call mpp_error(FATAL, 'OCEAN tracer field value error for profile_type=profile')
+        end do
+     end do
+  end do
+
+  !-- ATM profile_type=profile --!
+  tracer_index=get_tracer_index(MODEL_ATMOS, 'immadeup')
+  call set_tracer_profile(MODEL_ATMOS,tracer_index,tracer_out2,err_message)
+  !> answer
+  success=fm_get_value("/atmos_mod/tracer/immadeup/profile_type/profile/top_value", top_value)
+  success=fm_get_value("/atmos_mod/tracer/immadeup/profile_type/profile/surface_value", surf_value)
+  multiplier = exp( log (top_value/surf_value) /real(numlevels-1,r8_kind))
+  answer2(:,:,1)=surf_value
+  do i=2,numlevels
+     answer2(:,:,i) = answer2(:,:,i-1)*multiplier
+  end do
+  !> check results
+  do k=1, numlevels
+     do j=1, npoints
+        do i=1, npoints
+           if( tracer_out2(i,j,k) .ne. answer2(i,j,k)) &
+                call mpp_error(FATAL, 'ATM tracer field value error for profile_type=profile')
+        end do
+     end do
+  end do
+
+
+end subroutine test_set_tracer_profile
+
+end program test_tracer_manager
---
+++ test_fms/tracer_manager/test_tracer_manager2.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+
+#***********************************************************************
+#*                   GNU Lesser General Public License
+#*
+#* This file is part of the GFDL Flexible Modeling System (FMS).
+#*
+#* FMS is free software: you can redistribute it and/or modify it under
+#* the terms of the GNU Lesser General Public License as published by
+#* the Free Software Foundation, either version 3 of the License, or (at
+#* your option) any later version.
+#*
+#* FMS is distributed in the hope that it will be useful, but WITHOUT
+#* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#* for more details.
+#*
+#* You should have received a copy of the GNU Lesser General Public
+#* License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+#***********************************************************************
+
+# This is part of the GFDL FMS package. This is a shell script to
+# execute tests in the test_fms/field_manager directory.
+
+# Ed Hartnett 11/29/19
+
+# Set common test settings.
+. ../test-lib.sh
+
+# Copy files for test.
+cat <<_EOF > field_table
+# Simplified field table to run the field table unit tests
+ "TRACER", "ocean_mod", "biotic1"
+           "diff_horiz", "linear", "slope=ok"
+           "longname", "biotic one" /
+ "TRACER", "ocean_mod", "age_ctl" /
+ "TRACER", "ocean_mod" "immadeup2"
+           "longname", "im_made_up2_for_testing"
+           "units", "atomic_units"
+           "profile_type", "profile", "surface_value=1.e-12,bottom_value=1.e-9"/
+ "TRACER", "atmos_mod","radon"
+           "longname","radon-222"
+           "units","VMR*1E21"
+           "profile_type","fixed","surface_value=0.0E+00"
+           "convection","all"/
+ "TRACER", "atmos_mod" "immadeup"
+           "longname", "im_made_up_for_testing"
+           "units", "hbar"
+           "profile_type", "profile", "surface_value=1.e-12,top_value=1.e-15"/
+ "TRACER", "land_mod", "sphum"
+           "longname",     "specific humidity"
+            "units",        "kg/kg" /
+_EOF
+
+cat <<_EOF > field_table.yaml
+field_table:
+- field_type: tracer
+  modlist:
+  - model_type: atmos_mod
+    varlist:
+    - variable: radon
+      longname: radon-222
+      units: VMR*1E21
+      profile_type: fixed
+      subparams:
+      - surface_value: 0.0E+00
+      convection: all
+  - model_type: atmos_mod
+    varlist:
+    - variable: immadeup
+      longname: im_made_up_for_testing
+      units: hbar
+      profile_type: profile
+      subparams:
+      - surface_value: 1.0E-12
+      - top_value: 1.0E-15
+  - model_type: ocean_mod
+    varlist:
+    - variable: biotic1
+      diff_horiz: linear
+      subparams:
+      - slope: ok
+      longname: biotic one
+    - variable: age_ctl
+  - model_type: ocean_mod
+    varlist:
+    - variable: immadeup2
+      longname: im_made_up2_for_testing
+      units: hbar
+      profile_type: profile
+      subparams:
+      - surface_value: 1.0E-12
+      - bottom_value: 1.0E-9
+  - model_type: land_mod
+    varlist:
+    - variable: sphum
+      longname: specific humidity
+      units: kg/kg
+_EOF
+
+cat <<_EOF > input.nml
+&test_field_manager
+
+/
+_EOF
+
+test_expect_success "tracer_manager" 'mpirun -n 2 ./test_tracer_manager'
+
+test_done
