2eee5b0424088fc2f12c1a713c3fdf122853124a
Uriel Ramirez <uramirez8707@gmail.com>
2023-05-19 13:58:47 -0400

    minor documentation updates

--- test_fms/fms2_io/test_domain_io.F90
+++ test_fms/fms2_io/test_domain_io.F90
@@ -29,7 +29,8 @@ program test_domain_read
 
   implicit none
 
-  type data_type
+  !> @brief Dummy type to hold variable data
+  type data_t
     real(kind=r4_kind),    allocatable   :: var_r4(:,:,:,:,:)
     real(kind=r8_kind),    allocatable   :: var_r8(:,:,:,:,:)
     integer(kind=i4_kind), allocatable   :: var_i4(:,:,:,:,:)
@@ -52,8 +53,8 @@ program test_domain_read
   type(domain2d)                        :: Domain              !< Domain with mask table
   type(FmsNetcdfDomainFile_t)           :: fileobj             !< fms2io fileobj for domain decomposed
   character(len=6), dimension(5)        :: names               !< Dimensions names
-  type(data_type)                       :: var_data_in         !< Variable data written in
-  type(data_type)                       :: var_data_out        !< Variable data read in
+  type(data_t)                          :: var_data_in         !< Variable data written in
+  type(data_t)                          :: var_data_out        !< Variable data read in
   logical, allocatable, dimension(:,:)  :: parsed_mask         !< Parsed masked
   integer                               :: io                  !< Error code when reading namelist
   integer                               :: ierr                !< Error code when reading namelist
@@ -128,23 +129,23 @@ program test_domain_read
 
   !> @brief registers all of the possible variable types for a given
   !! number of dimensions
-  subroutine register_field_wrapper(fileobj, var_name, dimension_names, ndim)
-    type(FmsNetcdfDomainFile_t), intent(inout) :: fileobj            !< fms2io fileobj for domain decomposed
+  subroutine register_field_wrapper(fileob, var_name, dimension_names, ndim)
+    type(FmsNetcdfDomainFile_t), intent(inout) :: fileob             !< fms2io fileobj for domain decomposed
     character(len=*),            intent(in)    :: var_name           !< Name of the variable
     character(len=*),            intent(in)    :: dimension_names(:) !< dimension names
     integer,                     intent(in)    :: ndim               !< Number of dimension
 
-    call register_field(fileobj, trim(var_name)//"_r8", "double", names(1:ndim))
-    call register_field(fileobj, trim(var_name)//"_r4", "float",  names(1:ndim))
-    call register_field(fileobj, trim(var_name)//"_i8", "int",    names(1:ndim))
-    call register_field(fileobj, trim(var_name)//"_i4", "int64",  names(1:ndim))
+    call register_field(fileob, trim(var_name)//"_r8", "double", names(1:ndim))
+    call register_field(fileob, trim(var_name)//"_r4", "float",  names(1:ndim))
+    call register_field(fileob, trim(var_name)//"_i8", "int",    names(1:ndim))
+    call register_field(fileob, trim(var_name)//"_i4", "int64",  names(1:ndim))
   end subroutine register_field_wrapper
 
   !> @brief Allocates the variable to be the size of data compute domain for x and y
   !! and for a given size for the 3rd 4th and 5th dimension
-  subroutine var_data_alloc(var_data, Domain, dim3, dim4, dim5)
-    type(data_type),             intent(inout)    :: var_data         !< Variable data
-    type(domain2d),              intent(in)       :: Domain           !< Domain with mask table
+  subroutine var_data_alloc(var_data, var_domain, dim3, dim4, dim5)
+    type(data_t),                intent(inout)    :: var_data         !< Variable data
+    type(domain2d),              intent(in)       :: var_domain       !< Domain with mask table
     integer,                     intent(in)       :: dim3             !< Size of dim3
     integer,                     intent(in)       :: dim4             !< Size of dim4
     integer,                     intent(in)       :: dim5             !< Size of dim5
@@ -154,7 +155,7 @@ program test_domain_read
     integer :: js !< Starting y index
     integer :: je !< Ending y index
 
-    call mpp_get_data_domain(Domain, is, ie, js, je) !< This includes halos (but halos will not be written!)
+    call mpp_get_data_domain(var_domain, is, ie, js, je) !< This includes halos (but halos will not be written!)
 
     allocate(var_data%var_r4(is:ie, js:je, dim3, dim4, dim5))
     allocate(var_data%var_r8(is:ie, js:je, dim3, dim4, dim5))
@@ -164,7 +165,7 @@ program test_domain_read
 
   !> @brief Initializes the data to -999.99 for reals and -999 for integers
   subroutine var_data_init(var_data)
-    type(data_type),             intent(inout)    :: var_data         !< Variable data
+    type(data_t),             intent(inout)    :: var_data         !< Variable data
 
     var_data%var_r4 = real(-999.99, kind=r4_kind)
     var_data%var_r8 = real(-999.99, kind=r8_kind)
@@ -173,22 +174,22 @@ program test_domain_read
   end subroutine var_data_init
 
   !> @brief Sets the dcompute domain part of the variable to the expected number
-  subroutine var_data_set(var_data, domain, dim3, dim4, dim5)
-    type(data_type),             intent(inout)    :: var_data         !< Variable data
-    type(domain2d),              intent(in)       :: Domain           !< Domain with mask table
+  subroutine var_data_set(var_data, var_domain, dim3, dim4, dim5)
+    type(data_t),                intent(inout)    :: var_data         !< Variable data
+    type(domain2d),              intent(in)       :: var_domain       !< Domain with mask table
     integer,                     intent(in)       :: dim3             !< Size of dim3
     integer,                     intent(in)       :: dim4             !< Size of dim4
     integer,                     intent(in)       :: dim5             !< Size of dim5
 
-    integer :: i, j, k, l, m
+    integer :: i, j, k, l, m !< For do loops
 
     integer :: is !< Starting x index
     integer :: ie !< Ending x index
     integer :: js !< Starting y index
     integer :: je !< Ending y index
-    integer :: var_count
+    integer :: var_count !< For keeping track of the varible's data
 
-    call mpp_get_compute_domain(Domain, is, ie, js, je) !< This does not include halos!
+    call mpp_get_compute_domain(var_domain, is, ie, js, je) !< This does not include halos!
 
     var_count = 0
     do i = is, ie
@@ -210,78 +211,78 @@ program test_domain_read
   end subroutine
 
   !> @brief Writes the data for a give variable type
-  subroutine write_data_wrapper(fileobj, var_kind, var_data)
-    type(FmsNetcdfDomainFile_t), intent(inout) :: fileobj             !< fms2io fileobj for domain decomposed
+  subroutine write_data_wrapper(fileob, var_kind, var_data)
+    type(FmsNetcdfDomainFile_t), intent(inout) :: fileob              !< fms2io fileobj for domain decomposed
     character(len=*),            intent(in)    :: var_kind            !< The kind of the variable
     class(*),                    intent(in)    :: var_data(:,:,:,:,:) !< Variable data
 
-    call write_data(fileobj, "var2_"//trim(var_kind), var_data(:,:,1,1,1))
-    call write_data(fileobj, "var3_"//trim(var_kind), var_data(:,:,:,1,1))
-    call write_data(fileobj, "var4_"//trim(var_kind), var_data(:,:,:,:,1))
-    call write_data(fileobj, "var5_"//trim(var_kind), var_data(:,:,:,:,:))
+    call write_data(fileob, "var2_"//trim(var_kind), var_data(:,:,1,1,1))
+    call write_data(fileob, "var3_"//trim(var_kind), var_data(:,:,:,1,1))
+    call write_data(fileob, "var4_"//trim(var_kind), var_data(:,:,:,:,1))
+    call write_data(fileob, "var5_"//trim(var_kind), var_data(:,:,:,:,:))
 
   end subroutine
 
   !> @brief Reads the data and compares the checksum from the expected result
-  subroutine read_data_wrapper(fileobj, var_name, dim, var_data, ref_data)
-    type(FmsNetcdfDomainFile_t), intent(inout) :: fileobj             !< fms2io fileobj for domain decomposed
+  subroutine read_data_wrapper(fileob, var_name, dim, var_data, ref_data)
+    type(FmsNetcdfDomainFile_t), intent(inout) :: fileob              !< fms2io fileobj for domain decomposed
     character(len=*),            intent(in)    :: var_name            !< The kind of the variable
-    integer,                     intent(in)    :: dim
-    type(data_type),             intent(inout) :: var_data
-    type(data_type),             intent(inout) :: ref_data
+    integer,                     intent(in)    :: dim                 !< The variable's dimension
+    type(data_t),                intent(inout) :: var_data            !< The variable's data
+    type(data_t),                intent(inout) :: ref_data            !< The variable's reference data
 
     select case(dim)
     case(2)
       call var_data_init(var_data)
-      call read_data(fileobj, trim(var_name)//"_r4", var_data%var_r4(:,:,1,1,1))
+      call read_data(fileob, trim(var_name)//"_r4", var_data%var_r4(:,:,1,1,1))
       call compare_var_data(mpp_chksum(var_data%var_r4(:,:,1,1,1)), mpp_chksum(ref_data%var_r4(:,:,1,1,1)), "var2_r4")
 
-      call read_data(fileobj, trim(var_name)//"_r8", var_data%var_r8(:,:,1,1,1))
+      call read_data(fileob, trim(var_name)//"_r8", var_data%var_r8(:,:,1,1,1))
       call compare_var_data(mpp_chksum(var_data%var_r8(:,:,1,1,1)), mpp_chksum(ref_data%var_r8(:,:,1,1,1)), "var2_r8")
 
-      call read_data(fileobj, trim(var_name)//"_i4", var_data%var_i4(:,:,1,1,1))
+      call read_data(fileob, trim(var_name)//"_i4", var_data%var_i4(:,:,1,1,1))
       call compare_var_data(mpp_chksum(var_data%var_i4(:,:,1,1,1)), mpp_chksum(ref_data%var_i4(:,:,1,1,1)), "var2_i4")
 
-      call read_data(fileobj, trim(var_name)//"_i8", var_data%var_i8(:,:,1,1,1))
+      call read_data(fileob, trim(var_name)//"_i8", var_data%var_i8(:,:,1,1,1))
       call compare_var_data(mpp_chksum(var_data%var_i8(:,:,1,1,1)), mpp_chksum(ref_data%var_i8(:,:,1,1,1)), "var2_i8")
     case(3)
       call var_data_init(var_data)
-      call read_data(fileobj, trim(var_name)//"_r4", var_data%var_r4(:,:,:,1,1))
+      call read_data(fileob, trim(var_name)//"_r4", var_data%var_r4(:,:,:,1,1))
       call compare_var_data(mpp_chksum(var_data%var_r4(:,:,:,1,1)), mpp_chksum(ref_data%var_r4(:,:,:,1,1)), "var3_r4")
 
-      call read_data(fileobj, trim(var_name)//"_r8", var_data%var_r8(:,:,:,1,1))
+      call read_data(fileob, trim(var_name)//"_r8", var_data%var_r8(:,:,:,1,1))
       call compare_var_data(mpp_chksum(var_data%var_r8(:,:,:,1,1)), mpp_chksum(ref_data%var_r8(:,:,:,1,1)), "var3_r8")
 
-      call read_data(fileobj, trim(var_name)//"_i4", var_data%var_i4(:,:,:,1,1))
+      call read_data(fileob, trim(var_name)//"_i4", var_data%var_i4(:,:,:,1,1))
       call compare_var_data(mpp_chksum(var_data%var_i4(:,:,:,1,1)), mpp_chksum(ref_data%var_i4(:,:,:,1,1)), "var3_i4")
 
-      call read_data(fileobj, trim(var_name)//"_i8", var_data%var_i8(:,:,:,1,1))
+      call read_data(fileob, trim(var_name)//"_i8", var_data%var_i8(:,:,:,1,1))
       call compare_var_data(mpp_chksum(var_data%var_i8(:,:,:,1,1)), mpp_chksum(ref_data%var_i8(:,:,:,1,1)), "var3_i8")
     case(4)
       call var_data_init(var_data)
-      call read_data(fileobj, trim(var_name)//"_r4", var_data%var_r4(:,:,:,:,1))
+      call read_data(fileob, trim(var_name)//"_r4", var_data%var_r4(:,:,:,:,1))
       call compare_var_data(mpp_chksum(var_data%var_r4(:,:,:,:,1)), mpp_chksum(ref_data%var_r4(:,:,:,:,1)), "var4_r4")
 
-      call read_data(fileobj, trim(var_name)//"_r8", var_data%var_r8(:,:,:,:,1))
+      call read_data(fileob, trim(var_name)//"_r8", var_data%var_r8(:,:,:,:,1))
       call compare_var_data(mpp_chksum(var_data%var_r8(:,:,:,:,1)), mpp_chksum(ref_data%var_r8(:,:,:,:,1)), "var4_r8")
 
-      call read_data(fileobj, trim(var_name)//"_i4", var_data%var_i4(:,:,:,:,1))
+      call read_data(fileob, trim(var_name)//"_i4", var_data%var_i4(:,:,:,:,1))
       call compare_var_data(mpp_chksum(var_data%var_i4(:,:,:,:,1)), mpp_chksum(ref_data%var_i4(:,:,:,:,1)), "var4_i4")
 
-      call read_data(fileobj, trim(var_name)//"_i8", var_data%var_i8(:,:,:,:,1))
+      call read_data(fileob, trim(var_name)//"_i8", var_data%var_i8(:,:,:,:,1))
       call compare_var_data(mpp_chksum(var_data%var_i8(:,:,:,:,1)), mpp_chksum(ref_data%var_i8(:,:,:,:,1)), "var4_i8")
     case(5)
       call var_data_init(var_data)
-      call read_data(fileobj, trim(var_name)//"_r4", var_data%var_r4(:,:,:,:,:))
+      call read_data(fileob, trim(var_name)//"_r4", var_data%var_r4(:,:,:,:,:))
       call compare_var_data(mpp_chksum(var_data%var_r4(:,:,:,:,:)), mpp_chksum(ref_data%var_r4(:,:,:,:,:)), "var5_r4")
 
-      call read_data(fileobj, trim(var_name)//"_r8", var_data%var_r8(:,:,:,:,:))
+      call read_data(fileob, trim(var_name)//"_r8", var_data%var_r8(:,:,:,:,:))
       call compare_var_data(mpp_chksum(var_data%var_r8(:,:,:,:,:)), mpp_chksum(ref_data%var_r8(:,:,:,:,:)), "var5_r8")
 
-      call read_data(fileobj, trim(var_name)//"_i4", var_data%var_i4(:,:,:,:,:))
+      call read_data(fileob, trim(var_name)//"_i4", var_data%var_i4(:,:,:,:,:))
       call compare_var_data(mpp_chksum(var_data%var_i4(:,:,:,:,:)), mpp_chksum(ref_data%var_i4(:,:,:,:,:)), "var5_i4")
 
-      call read_data(fileobj, trim(var_name)//"_i8", var_data%var_i8(:,:,:,:,:))
+      call read_data(fileob, trim(var_name)//"_i8", var_data%var_i8(:,:,:,:,:))
       call compare_var_data(mpp_chksum(var_data%var_i8(:,:,:,:,:)), mpp_chksum(ref_data%var_i8(:,:,:,:,:)), "var5_i8")
     end select
 
@@ -289,9 +290,9 @@ program test_domain_read
 
   !> @brief Compares two checksums and crashes if they are not the same
   subroutine compare_var_data(check_sum_in, check_sum_ref, varname)
-    integer(kind=i8_kind), intent(in) :: check_sum_in
-    integer(kind=i8_kind), intent(in) :: check_sum_ref
-    character(len=*), intent(in) :: varname
+    integer(kind=i8_kind), intent(in) :: check_sum_in   !< Checksum
+    integer(kind=i8_kind), intent(in) :: check_sum_ref  !< The reference checksum
+    character(len=*),      intent(in) :: varname        !< The variable's name (for error messages)
 
    if (check_sum_ref .ne. check_sum_in) call mpp_error(FATAL, &
      "Checksums do not match for variable: "//trim(varname))
