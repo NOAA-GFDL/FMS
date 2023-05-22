d9e7cb0006015b6bd71cbceeccd707d9c40b0113
Jesse Lentz <jesse.lentz@noaa.gov>
2023-05-22 12:42:51 -0400

    Use standard deviations of sample moments
    Rather than requiring that sample moments be within 5% of their expected values,
    calculate the standard deviations of sample moments and require that each sample
    moment be within one standard deviation of its expected value.

--- test_fms/random_numbers/test_random_numbers.F90
+++ test_fms/random_numbers/test_random_numbers.F90
@@ -9,8 +9,16 @@ use random_numbers_mod
 
 implicit none
 
+integer, parameter :: n_moments = 1000 !< Highest-order moment to test
+integer, parameter :: n0_1d = 2500 !< Initial length of 1D random sample vector
+integer, parameter :: n0_2d = 50 !< Initial dimensions of 2D random sample array
+
 integer, parameter :: seeds(0:*) = [0, -5, 3] !< Seed constants
-integer, parameter :: k = KIND_ !< Precision of the test: either r4_kind or r8_kind
+integer, parameter :: k = KIND_ !< Either r4_kind or r8_kind
+
+real(k), dimension(n_moments) :: moment_mu !< Ideal moment values
+real(k), dimension(n_moments) :: moment_sigma !< Standard deviations of sample moments
+
 type(time_type) :: now !< Current date and time
 
 call fms_init
@@ -27,15 +35,26 @@ call fms_end
 
 contains
 
+#define CALC_MOMENT_(i) (1._k / real(i + 1, k))
+
+subroutine calc_ideal_moments
+  integer :: i !< Moment order
+  integer, parameter :: n = n0_1d !< Sample size for which to calculate moment standard deviations
+
+  do i=1, n_moments
+    moment_mu(i) = CALC_MOMENT_(i)
+    moment_sigma(i) = sqrt((CALC_MOMENT_(2*i) - moment_mu(i)**2) / n)
+  enddo
+end subroutine calc_ideal_moments
+
 subroutine set_time
   integer :: now_values(8) !< Values returned by the date_and_time() intrinsic
 
   call set_calendar_type(JULIAN)
   call date_and_time(values=now_values)
 
-  now = set_date(2023, 4, 28)
-!  now = set_date(now_values(1), now_values(2), now_values(3), &
-!                 now_values(5), now_values(6), now_values(7))
+  now = set_date(now_values(1), now_values(2), now_values(3), &
+                 now_values(5), now_values(6), now_values(7))
 end subroutine set_time
 
 subroutine test_constructSeed
@@ -78,6 +97,8 @@ end subroutine constructSeed_assert
 subroutine test_getRandomNumbers
   type(randomNumberStream) :: stream !< Random number stream
 
+  call calc_ideal_moments
+
   select case (mpp_pe())
     case (0)
       stream = initializeRandomNumberStream(seeds(0))
@@ -97,9 +118,6 @@ subroutine test_getRandomNumbers
 end subroutine test_getRandomNumbers
 
 subroutine test_getRandomNumbers_dispatch(stream)
-  integer, parameter :: n0_1d = 400 !< Initial length of 1D random sample vector
-  integer, parameter :: n0_2d = 20 !< Initial dimensions of 2D random sample array
-
   type(randomNumberStream), intent(inout) :: stream !< Random number stream
 
   call test_samples_iter(stream, test_sample_1d, n0_1d)
@@ -107,15 +125,15 @@ subroutine test_getRandomNumbers_dispatch(stream)
 end subroutine test_getRandomNumbers_dispatch
 
 subroutine test_samples_iter(stream, test, n0)
-  integer, parameter :: required_passes = 5 !< Number of samples that must pass for each seed value
+  integer, parameter :: required_passes = 10 !< Number of samples that must pass for each seed value
 
   type(randomNumberStream), intent(inout) :: stream !< Random number stream
   procedure(test_sample_1d) :: test !< Procedure to draw a random sample and test it
   integer, intent(in) :: n0 !< Initial sample size to test
 
   real(k) :: x !< Sample for 0D test
-  integer :: n !< Sample size
-  integer :: pass_counter !< Number of passes
+  integer :: n !< Sample size for 1D or 2D test
+  integer :: pass_counter !< Number of test passes
 
   ! 0D case
   ! Get a scalar and check that it's within [0,1]
@@ -123,8 +141,9 @@ subroutine test_samples_iter(stream, test, n0)
   call check_bounds(x)
 
   ! 1D and 2D cases
-  ! Attempt to draw five samples for which the first 1,000 moments are within
-  ! 5% of the expected values for the uniform distribution on [0,1]
+  ! Attempt to draw ten samples for which the first 1,000 moments are within
+  ! one standard deviation of the expected values for the uniform distribution
+  ! on [0,1]
 
   n = n0
   pass_counter = 0
@@ -172,21 +191,16 @@ function test_sample_2d(stream, n)
   test_sample_2d = compare_sample_moments(reshape(arr, [size(arr)]))
 end function test_sample_2d
 
-! Check that the first 1000 moments are consistent with the uniform distribution
-! in the interval [0,1]
+! Check that the first 1,000 moments are within one standard deviation of their expected values.
 function compare_sample_moments(v)
-  real(k), parameter :: tolerance = .05_k !< Tolerance, expressed as a fraction of the ideal moment value
-  integer, parameter :: n_moments = 1000 !< Highest-order moment to be tested
-
   real(k), intent(in) :: v(:) !< Vector containing a random sample
   logical :: compare_sample_moments !< True if the sample passes the test
 
   real(k), allocatable :: vi(:) !< v(:) raised to the power i
-  integer :: i !< Loop counter
+  integer :: i !< Moment order
   integer :: n !< Size of v(:)
-  real(k) :: moment_ideal !< Ideal value for the i-th moment
-  real(k) :: moment_sample !< Actual value of the i-th moment, calculated from v(:)
-  real(k) :: eps !< Acceptable deviation from the ideal moment value
+
+  real(k) :: moment_sample !< Value of the i-th sample moment, calculated from v(:)
 
   n = size(v)
   allocate(vi(n))
@@ -195,10 +209,8 @@ function compare_sample_moments(v)
   do i = 1, n_moments
     vi = vi * v
     moment_sample = sum(vi) / n
-    moment_ideal = 1._k / (i + 1)
-    eps = tolerance * moment_ideal
 
-    if (abs(moment_sample - moment_ideal) .gt. eps) then
+    if (abs(moment_sample - moment_mu(i)) .gt. moment_sigma(i)) then
       compare_sample_moments = .false.
       return
     endif
@@ -207,8 +219,7 @@ function compare_sample_moments(v)
   compare_sample_moments = .true.
 end function compare_sample_moments
 
-! Check that a value lies in the [0, 1] range. A single out-of-bounds number
-! will cause the test to fail.
+! Check that a value lies within the 0<x<1 range
 subroutine check_bounds(x)
   real(k), intent(in) :: x !< Scalar value to check
 
