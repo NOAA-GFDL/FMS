ab6696e967f7808bc48f66bf519f9ec88195e083
ganganoaa <121043264+ganganoaa@users.noreply.github.com>
2023-06-08 11:10:53 -0400

    Update fms_diag_object.F90

--- diag_manager/fms_diag_object.F90
+++ diag_manager/fms_diag_object.F90
@@ -583,9 +583,9 @@ CALL MPP_ERROR(FATAL,"You can not use the modern diag manager without compiling 
       !!TODO: Get the vertical layer start and end indices
 
       !> Check if time is not present for fields output every time step
-      if (freq == EVERY_TIME .and. .not.this%FMS_diag_fields(diag_field_id)%is_static()) then
-        if (this%FMS_diag_files(file_id)%FMS_diag_file%next_output == &
-          this%FMS_diag_files(file_id)%FMS_diag_file%last_output) then
+      if (all(freq) == EVERY_TIME .and. .not.this%FMS_diag_fields(diag_field_id)%is_static()) then
+        if (this%FMS_diag_files(file_id)%FMS_diag_file%get_next_output == &
+          this%FMS_diag_files(file_id)%FMS_diag_file%get_last_output) then
           if (.not.present(time)) then
             write (error_string,'(a,"/",a)') trim(this%FMS_diag_fields(diag_field_id)%get_modname()),&
               trim(this%FMS_diag_fields(diag_field_id)%diag_field(i)%get_var_outname())
