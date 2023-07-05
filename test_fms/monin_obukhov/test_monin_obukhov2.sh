c7585bc463839cc67638d4ea90165008d812cdf8
Jesse Lentz <jesse.lentz@noaa.gov>
2023-07-05 10:53:22 -0400

    Use absolute paths when copying nml files

--- test_fms/monin_obukhov/test_monin_obukhov2.sh
+++ test_fms/monin_obukhov/test_monin_obukhov2.sh
@@ -28,7 +28,7 @@
 # Run tests
 for p in r4 r8
 do
-  cp input.${p}.nml input.nml
+  cp ${top_srcdir}/test_fms/monin_obukhov/input.${p}.nml input.nml
   test_expect_success "test monin_obukhov_mod (${p})" "mpirun -n 1 ./test_monin_obukhov_${p}"
 done
 
