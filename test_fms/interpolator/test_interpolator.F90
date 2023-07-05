dbed2ceb9c9acfd392eb18bd00e758cde31b55df
Ryan Mulhall <35538242+rem1776@users.noreply.github.com>
2023-07-05 13:50:17 -0400

    fix: test errors from io flag (#1271)

--- test_fms/interpolator/test_interpolator.F90
+++ test_fms/interpolator/test_interpolator.F90
@@ -37,7 +37,11 @@ program test_interpolator
 
 use mpp_mod
 use mpp_domains_mod
+#ifdef use_deprecated_io
+use fms_mod, old_open_file => open_file
+#else
 use fms_mod
+#endif
 use time_manager_mod
 use diag_manager_mod
 use interpolator_mod
