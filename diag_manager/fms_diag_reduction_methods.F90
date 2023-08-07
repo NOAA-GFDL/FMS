072c9e6cd66d5e6218fb5b085e20023f41cc5b5b
Uriel Ramirez <uramirez8707@gmail.com>
2023-08-07 14:13:33 -0400

    add the fms_diag_reduction_methods_module

---
+++ diag_manager/fms_diag_reduction_methods.F90
@@ -0,0 +1,9 @@
+!> \author Ganga Purja Pun
+!> \email GFDL.Climate.Model.Info@noaa.gov
+!! \brief Contains routines for the modern diag manager
+!! These routines are meant to be used for checks and in reduction methods.
+
+module fms_diag_reduction_methods_mod
+  implicit none
+
+end module fms_diag_reduction_methods_mod
