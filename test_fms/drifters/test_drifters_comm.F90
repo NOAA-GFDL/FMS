7bc1bdc3b70e4454015359bf90eabd06c8b489a3
Caitlyn Mcallister <caitlyn.mcallister@noaa.gov>
2023-08-14 12:21:41 -0400

    remove whitespace

--- test_fms/drifters/test_drifters_comm.F90
+++ test_fms/drifters/test_drifters_comm.F90
@@ -19,7 +19,7 @@
 !**********************************************************************
 
 program test_drifters_comm
-#ifdef use_drifters     
+#ifdef use_drifters
 
   use drifters_core_mod
   use drifters_comm_mod
@@ -143,6 +143,6 @@ program test_drifters_comm
   call drifters_comm_del(drfts_com)
   call mpp_domains_exit
   call mpp_exit
-  
+
 #endif
 end program test_drifters_comm
