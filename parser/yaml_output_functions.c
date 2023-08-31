e0ca287c2b3e18a5fa2c8719e12b6c9ad09cb9c0
rem1776 <Ryan.Mulhall@noaa.gov>
2023-08-31 17:05:00 -0400

    fix for parser test and coupler, remove skips

--- parser/yaml_output_functions.c
+++ parser/yaml_output_functions.c
@@ -363,7 +363,7 @@ void write_yaml_from_struct_3 (char *yamlname, int asize, struct fmsyamloutkeys 
 
   // trim any trailing whitespace
   int ws_ind = strlen(yamlname)-1;
-  while(*(yamlname+ws_ind) == ' ') ws_ind--;
+  while(!isalnum(yamlname[ws_ind])) ws_ind--;
   if( ws_ind != strlen(yamlname)-1) yamlname[ws_ind+1] = '\0';
 
   /* open the yaml output file. Only 1 core should do this */
