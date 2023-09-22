97ad65c016e3b65caef43ef307a23c0e1d36a871
Uriel Ramirez <uramirez8707@gmail.com>
2023-09-22 13:23:16 -0400

    Add tile number to the filename for subregional files

--- diag_manager/diag_output.F90
+++ diag_manager/diag_output.F90
@@ -111,6 +111,8 @@ CONTAINS
     integer, allocatable, dimension(:) :: current_pelist
     integer :: mype  !< The pe you are on
     character(len=9) :: mype_string !< a string to store the pe
+    character(len=128) :: tmp_filename      !< Temp variable to store the file_name
+
     !---- initialize mpp_io ----
     IF ( .NOT.module_is_initialized ) THEN
        module_is_initialized = .TRUE.
@@ -135,8 +137,11 @@ CONTAINS
        fileob => fileobjND
        mype = mpp_pe()
        write(mype_string,'(I0.4)') mype
+       call get_mosaic_tile_file(file_name, tmp_filename, .true., domain)
+       tmp_filename = trim(tmp_filename)//"."//trim(mype_string)
+
         if (.not.check_if_open(fileob)) then
-               call open_check(open_file(fileobjND, trim(file_name)//".nc."//trim(mype_string), "overwrite", &
+               call open_check(open_file(fileobjND, trim(tmp_filename), "overwrite", &
                             is_restart=.false.))
                !< For regional subaxis add the NumFilesInSet attribute, which is added by fms2_io for (other)
                !< domains with sufficient decomposition info. Note mppnccombine will work with an entry of zero.
