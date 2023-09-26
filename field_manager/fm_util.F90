5210f1051a7c41f590826278cd017e40d2654882
Caitlyn Mcallister <caitlyn.mcallister@noaa.gov>
2023-09-26 10:37:39 -0400

    change `value` to `sval` in routine fm_util_set_value_string

--- field_manager/fm_util.F90
+++ field_manager/fm_util.F90
@@ -2403,7 +2403,7 @@ end subroutine fm_util_set_value_logical  !}
 
 !#######################################################################
 !> Set a string value in the Field Manager tree.
-subroutine fm_util_set_value_string(name, value, caller, index, append, no_create,        &
+subroutine fm_util_set_value_string(name, sval, caller, index, append, no_create,        &
      no_overwrite, good_name_list)  !{
 
 implicit none
@@ -2413,7 +2413,7 @@ implicit none
 !
 
 character(len=*), intent(in)            :: name
-character(len=*), intent(in)            :: value
+character(len=*), intent(in)            :: sval
 character(len=*), intent(in), optional  :: caller
 integer, intent(in), optional           :: index
 logical, intent(in), optional           :: append
@@ -2513,21 +2513,21 @@ if (present(index)) then  !{
       call mpp_error(FATAL, trim(error_header) // ' Problem getting length of ' // trim(name))
     endif  !}
     if (.not. (no_overwrite_use .and. field_length .ge. index)) then  !{
-      field_index = fm_new_value(name, value, index = index)
+      field_index = fm_new_value(name, sval, index = index)
       if (field_index .le. 0) then  !{
         write (str_error,*) ' with index = ', index
         call mpp_error(FATAL, trim(error_header) // ' Problem overwriting ' // trim(name) // trim(str_error))
       endif  !}
     endif  !}
   else  !}{
-    field_index = fm_new_value(name, value, index = index)
+    field_index = fm_new_value(name, sval, index = index)
     if (field_index .le. 0) then  !{
       write (str_error,*) ' with index = ', index
       call mpp_error(FATAL, trim(error_header) // ' Problem setting ' // trim(name) // trim(str_error))
     endif  !}
   endif  !}
 elseif (present(append)) then  !}{
-  field_index = fm_new_value(name, value, append = append)
+  field_index = fm_new_value(name, sval, append = append)
   if (field_index .le. 0) then  !{
     write (str_error,*) ' with append = ', append
     call mpp_error(FATAL, trim(error_header) // ' Problem setting ' // trim(name) // trim(str_error))
@@ -2535,13 +2535,13 @@ elseif (present(append)) then  !}{
 else  !}{
   if (fm_exists(name)) then  !{
     if (.not. no_overwrite_use) then  !{
-      field_index = fm_new_value(name, value)
+      field_index = fm_new_value(name, sval)
       if (field_index .le. 0) then  !{
         call mpp_error(FATAL, trim(error_header) // ' Problem overwriting ' // trim(name))
       endif  !}
     endif  !}
   elseif (create) then  !}{
-    field_index = fm_new_value(name, value)
+    field_index = fm_new_value(name, sval)
     if (field_index .le. 0) then  !{
       call mpp_error(FATAL, trim(error_header) // ' Problem creating ' // trim(name))
     endif  !}
