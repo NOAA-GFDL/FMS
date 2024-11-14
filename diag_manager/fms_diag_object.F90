23df6d7a905aaa9618e597b82c49b56009a0caa2
uramirez8707 <49168881+uramirez8707@users.noreply.github.com>
2024-11-14 10:54:44 -0500

    fix: change `fms_diag_accept_data` into a subroutine (#1610)

--- diag_manager/fms_diag_object.F90
+++ diag_manager/fms_diag_object.F90
@@ -537,7 +537,7 @@ end function fms_diag_axis_init
 !! multithreaded case.
 !! \note If some of the diag manager is offloaded in the future, then it should be treated similarly
 !! to the multi-threaded option for processing later
-logical function fms_diag_accept_data (this, diag_field_id, field_data, mask, rmask, &
+subroutine fms_diag_accept_data (this, diag_field_id, field_data, mask, rmask, &
                                        time, is_in, js_in, ks_in, &
                                        ie_in, je_in, ke_in, weight, err_msg)
   class(fmsDiagObject_type),TARGET,      INTENT(inout)          :: this          !< Diaj_obj to fill
@@ -681,8 +681,6 @@ CALL MPP_ERROR(FATAL,"You can not use the modern diag manager without compiling 
 !$omp end critical
     call this%FMS_diag_fields(diag_field_id)%set_data_buffer(field_data, oor_mask, field_weight, &
                                                              is, js, ks, ie, je, ke)
-    fms_diag_accept_data = .TRUE.
-    return
   else
 
     !< At this point if we are no longer in an openmp region or running with 1 thread
@@ -710,13 +708,10 @@ CALL MPP_ERROR(FATAL,"You can not use the modern diag manager without compiling 
     if(.not. this%FMS_diag_fields(diag_field_id)%has_mask_allocated()) &
       call this%FMS_diag_fields(diag_field_id)%allocate_mask(oor_mask)
     call this%FMS_diag_fields(diag_field_id)%set_mask(oor_mask, field_info)
-    return
   end if main_if
   !> Return false if nothing is done
-  fms_diag_accept_data = .FALSE.
-  return
 #endif
-end function fms_diag_accept_data
+end subroutine fms_diag_accept_data
 
 !< @brief Do the math for all the buffers
 subroutine do_buffer_math(this)
