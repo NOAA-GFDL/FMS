a5662f03290d82a3aec510f813fa87cd5aa7b9d8
MiKyung Lee <58964324+mlee03@users.noreply.github.com>
2024-12-19 10:44:56 -0500

    refactor: mosaic directory cleanup and rename to grid_utils (#1626)

--- grid_utils/constant.h
+++ grid_utils/constant.h
@@ -16,5 +16,13 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
  **********************************************************************/
-#define RADIUS        (6371000.)
-#define STRING        255
+#define RADIUS   (6371000.)
+#define STRING   255
+
+#define EPSLN8   (1.e-8)
+#define EPSLN15  (1.0e-15)
+#define EPSLN30  (1.0e-30)
+#define EPSLN10  (1.0e-10)
+#define R2D (180/M_PI)
+#define TPI (2.0*M_PI)
+#define HPI (0.5*M_PI)
---
+++ test_fms/horiz_interp/test_create_xgrid_order2.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#***********************************************************************
+#*                   GNU Lesser General Public License
+#*
+#* This file is part of the GFDL Flexible Modeling System (FMS).
+#*
+#* FMS is free software: you can redistribute it and/or modify it under
+#* the terms of the GNU Lesser General Public License as published by
+#* the Free Software Foundation, either version 3 of the License, or (at
+#* your option) any later version.
+#*
+#* FMS is distributed in the hope that it will be useful, but WITHOUT
+#* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#* for more details.
+#*
+#* You should have received a copy of the GNU Lesser General Public
+#* License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+#***********************************************************************
+
+# This is part of the GFDL FMS package. This is a shell script to
+# execute tests in the test_fms/horiz_interp directory.
+
+# Ed Hartnett 11/29/19
+# Ryan Mulhall 01/23
+
+# Set common test settings.
+. ../test-lib.sh
+
+
+test_expect_success "create_xgrid order2" 'mpirun -n 1 ./test_create_xgrid_order2_r8'
+test_done
