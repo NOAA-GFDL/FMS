bed59725844502cf09e1b532cae099fe68da4ea1
Jesse Lentz <Jesse.Lentz@noaa.gov>
2025-02-26 14:13:52 -0500

    Add GPL header to test_gex.F90

--- test_fms/coupler/test_gex.F90
+++ test_fms/coupler/test_gex.F90
@@ -1,6 +1,24 @@
-program test_coupler_gex
+!***********************************************************************
+!*                   GNU Lesser General Public License
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* FMS is free software: you can redistribute it and/or modify it under
+!* the terms of the GNU Lesser General Public License as published by
+!* the Free Software Foundation, either version 3 of the License, or (at
+!* your option) any later version.
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+!* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+!* for more details.
+!*
+!* You should have received a copy of the GNU Lesser General Public
+!* License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+!***********************************************************************
 
-! public :: gex_get_index,gex_get_n_ex, gex_get_property
+!> This program tests the public API of gex_mod.
+program test_coupler_gex
 
 use fms_mod, only: fms_init, fms_end, check_nml_error
 use mpp_mod, only: mpp_error, FATAL, input_nml_file
