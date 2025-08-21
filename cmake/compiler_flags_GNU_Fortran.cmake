b034f50bafef1c9a33d52b8368803188c46c8420
rem1776 <Ryan.Mulhall@noaa.gov>
2025-08-21 13:38:58 -0400

    missed the fortran max line length flag for GNU

--- cmake/compiler_flags_GNU_Fortran.cmake
+++ cmake/compiler_flags_GNU_Fortran.cmake
@@ -3,7 +3,7 @@ set(r8_flags "-fdefault-real-8 -fdefault-double-8") # Fortran flags for 64BIT pr
 set(r4_flags "-fdefault-real-4") # Fortran flags for 32BIT precision
 
 # GNU Fortran
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fcray-pointer -fallow-argument-mismatch")
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fcray-pointer -fallow-argument-mismatch -ffree-line-length-none")
 
 set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
 set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -g")
