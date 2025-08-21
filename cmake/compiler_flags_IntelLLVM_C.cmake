b9047272aa6d3fcdea1fca7fe7fe21cc548fc242
rem1776 <Ryan.Mulhall@noaa.gov>
2025-08-21 13:40:43 -0400

    linter

--- cmake/compiler_flags_IntelLLVM_C.cmake
+++ cmake/compiler_flags_IntelLLVM_C.cmake
@@ -2,7 +2,7 @@
 
 set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
 
-set( CMAKE_C_FLAGS_RELEASE "-O2") 
+set( CMAKE_C_FLAGS_RELEASE "-O2")
 set( CMAKE_C_FLAGS_DEBUG "-O0 -g -traceback")
 
 set( CMAKE_C_LINK_FLAGS "")
