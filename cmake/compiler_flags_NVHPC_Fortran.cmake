ba9b3c1a19700dcc6e808f510cb03aad9498dcb5
Jorge Luis Gálvez Vallejo <jorgegalvez1694@gmail.com>
2025-08-26 10:00:39 +1000

    Update cmake/compiler_flags_NVHPC_Fortran.cmake
    Co-authored-by: Ryan Mulhall <35538242+rem1776@users.noreply.github.com>

--- cmake/compiler_flags_NVHPC_Fortran.cmake
+++ cmake/compiler_flags_NVHPC_Fortran.cmake
@@ -4,7 +4,7 @@ set(r8_flags "-r8") # Fortran flags for 64BIT precision
 # NVHPC Fortan
 set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ")
 
-set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -fast")
+set(CMAKE_Fortran_FLAGS_RELEASE "-O3")
 
 set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -Ktrap=fp" )
 # -g can cause bugs, see: https://forums.developer.nvidia.com/t/bug-compiling-with-g-o0-produces-a-compute-sanitizer-error-removing-g-removes-the-error/341478 
