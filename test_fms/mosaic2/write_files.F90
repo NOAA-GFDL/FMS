c5c5b5fedb1c116ebd516b3bb350343fca9b3c86
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-09-10 17:51:32 +0200

    Undef define after its usage

--- test_fms/mosaic2/write_files.F90
+++ test_fms/mosaic2/write_files.F90
@@ -2,6 +2,7 @@ module write_files_r4
 
 #define TEST_FMS_KIND_ r4_kind
 #include "write_files.inc"
+#undef TEST_FMS_KIND_
 
 end module
 
@@ -10,5 +11,6 @@ module write_files_r8
 
 #define TEST_FMS_KIND_ r8_kind
 #include "write_files.inc"
+#undef TEST_FMS_KIND_
 
 end module
