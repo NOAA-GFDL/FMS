0aa11844fba39ff27bce84f426412d81ace9ceb7
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-09-22 13:52:30 -0400

    add license

--- test_fms/time_interp/test_time_interp_conservative_hi.F90
+++ test_fms/time_interp/test_time_interp_conservative_hi.F90
@@ -1,3 +1,21 @@
+!***********************************************************************
+!*                             Apache License 2.0
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* Licensed under the Apache License, Version 2.0 (the "License");
+!* you may not use this file except in compliance with the License.
+!* You may obtain a copy of the License at
+!*
+!*     http://www.apache.org/licenses/LICENSE-2.0
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied;
+!* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+!* PARTICULAR PURPOSE. See the License for the specific language
+!* governing permissions and limitations under the License.
+!***********************************************************************
+
 program test_time_interp_with_interp
 
   use constants_mod, only: DEG_TO_RAD
