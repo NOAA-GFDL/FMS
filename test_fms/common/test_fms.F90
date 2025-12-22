cdaf11f55a56a967adc029fc36fa917ff6640280
Jesse Lentz <jesse.lentz@noaa.gov>
2025-12-22 14:45:09 -0500

    Use Apache license for newly created files

--- test_fms/common/test_fms.F90
+++ test_fms/common/test_fms.F90
@@ -1,3 +1,21 @@
+!***********************************************************************
+!*                             Apache License 2.0
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* Licensed under the Apache License, Version 2.0 (the "License");
+!* you may not use this file except in compliance with the License.
+!* You may obtain a copy of the License at
+!*
+!*     http://www.apache.org/licenses/LICENSE-2.0
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied;
+!* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+!* PARTICULAR PURPOSE. See the License for the specific language
+!* governing permissions and limitations under the License.
+!***********************************************************************
+
 ! This module allows arrays to be permuted, and provides a data type for the
 ! purpose of storing permuted array bounds. It provides procedures for
 ! initializing a 2D or 3D array with random data, and for comparing a 2D or
