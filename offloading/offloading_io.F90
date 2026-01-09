596da47e335d42ef6e5882029b9241bf3c42e0cf
rem1776 <Ryan.Mulhall@noaa.gov>
2026-01-09 14:36:48 -0500

    add any missing apache licenses

--- offloading/offloading_io.F90
+++ offloading/offloading_io.F90
@@ -1,3 +1,20 @@
+!***********************************************************************
+!*                             Apache License 2.0
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* Licensed under the Apache License, Version 2.0 (the "License");
+!* you may not use this file except in compliance with the License.
+!* You may obtain a copy of the License at
+!*
+!*     http://www.apache.org/licenses/LICENSE-2.0
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied;
+!* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+!* PARTICULAR PURPOSE. See the License for the specific language
+!* governing permissions and limitations under the License.
+!***********************************************************************
 module offloading_io_mod
   use mpp_mod
   use mpp_domains_mod
@@ -26,7 +43,7 @@ module offloading_io_mod
     integer :: type_of_domain !< type of domain (domain_decomposed, non_domain_decomposed, Unstructured_grid)
   end type
 
-  !> Offload equivalent of open_file in fms2_io_mod
+  !> Offload equivalent of register_axis in fms2_io_mod
   !! Registers an axis to a netcdf file on offloaded PEs. File must have been opened with open_file_offload.
   interface register_axis_offload
     procedure :: register_netcdf_axis_offload
--- test_fms/offloading/test_metadata_transfer.F90
+++ test_fms/offloading/test_metadata_transfer.F90
@@ -1,3 +1,20 @@
+!***********************************************************************
+!*                             Apache License 2.0
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* Licensed under the Apache License, Version 2.0 (the "License");
+!* you may not use this file except in compliance with the License.
+!* You may obtain a copy of the License at
+!*
+!*     http://www.apache.org/licenses/LICENSE-2.0
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied;
+!* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+!* PARTICULAR PURPOSE. See the License for the specific language
+!* governing permissions and limitations under the License.
+!***********************************************************************
 program test_metadata_transfer
   use fms_mod, only: fms_init, fms_end, string
   use mpp_mod
